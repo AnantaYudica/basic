181fbeb4728c727e08136682ba066cc5ec9b92ba
AnantaYudica <nta040689@gmail.com>
2018-08-02 23:56:42 +0700

    Add file Simple.h  - Add template class Simple

---
+++ include/type/trait/Simple.h
@@ -0,0 +1,191 @@
+#ifndef TYPE_TRAIT_SIMPLE_H_
+#define TYPE_TRAIT_SIMPLE_H_
+
+#include <cstddef>
+
+namespace basic
+{
+namespace type
+{
+
+template<typename T>
+struct Simple
+{
+    typedef T Type;
+    typedef typename Simple<T>::Type type;
+};
+
+template<typename T>
+struct Simple<const T>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T>
+struct Simple<volatile T>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T>
+struct Simple<const volatile T>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T>
+struct Simple<T*>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+
+template<typename T, typename Tc>
+struct Simple<T Tc::*>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T, typename Tc>
+struct Simple<T Tc::*const>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T, typename Tc>
+struct Simple<T Tc::*volatile>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T, typename Tc>
+struct Simple<T Tc::*const volatile>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T>
+struct Simple<T&>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T>
+struct Simple<T&&>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T>
+struct Simple<T[]>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T, std::size_t N>
+struct Simple<T[N]>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T, typename... Targs>
+struct Simple<T(Targs...)>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T, typename... Targs>
+struct Simple<T(Targs...) &>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T, typename... Targs>
+struct Simple<T(Targs...) &&>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T, typename... Targs>
+struct Simple<T(Targs...) const>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T, typename... Targs>
+struct Simple<T(Targs...) const &>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T, typename... Targs>
+struct Simple<T(Targs...) const &&>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T, typename... Targs>
+struct Simple<T(Targs...) volatile>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T, typename... Targs>
+struct Simple<T(Targs...) volatile &>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T, typename... Targs>
+struct Simple<T(Targs...) volatile &&>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T, typename... Targs>
+struct Simple<T(Targs...) const volatile>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T, typename... Targs>
+struct Simple<T(Targs...) const volatile &>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+template<typename T, typename... Targs>
+struct Simple<T(Targs...) const volatile &&>
+{
+    typedef typename Simple<T>::Type Type;
+    typedef typename Simple<T>::type type;
+};
+
+} //!type
+
+} //!basic
+
+#endif //!TYPE_TRAIT_SIMPLE_H_
