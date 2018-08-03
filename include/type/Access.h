e7ba9f2ae319e40107648ca7158741e8b839c132
AnantaYudica <nta040689@gmail.com>
2018-08-03 21:33:28 +0700

    Update Mistake naming namespace

--- include/type/Access.h
+++ include/type/Access.h
@@ -24,12 +24,20 @@ struct Private : Access {};
 
 } //!access
 
+} //!type
+
+} //!basic
+
 namespace _helper
+{
+namespace _basic
+{
+namespace _type
 {
 namespace _is_baseof
 {
 
-__DEFINE_IS_BASE_OF_DEFAULT_(_Access, __Access, T,  Access);
+__DEFINE_IS_BASE_OF_DEFAULT_(_Access, __Access, T,  basic::type::Access);
 
 } //!_is_baseof
 
@@ -38,11 +46,11 @@ namespace _access
 namespace _is_same_with
 {
 
-__DEFINE_IS_SAME_WITH_DEFAULT_(_Public, T, access::Public);
+__DEFINE_IS_SAME_WITH_DEFAULT_(_Public, T, basic::type::access::Public);
 
-__DEFINE_IS_SAME_WITH_DEFAULT_(_Protected, T, access::Protected);
+__DEFINE_IS_SAME_WITH_DEFAULT_(_Protected, T, basic::type::access::Protected);
 
-__DEFINE_IS_SAME_WITH_DEFAULT_(_Private, T, access::Private);
+__DEFINE_IS_SAME_WITH_DEFAULT_(_Private, T, basic::type::access::Private);
 
 } //!_is_same_with
 
@@ -62,23 +70,32 @@ __DEFINE_ENABLE_IF_DEFAULT_(_Private, BoolTest, T, To,
 
 } //!_access
 
+} //!type
+
+} //!basic
+
 } //!_helper
 
+namespace basic
+{
+namespace type
+{
 namespace access
 {
 namespace enable_if
 {
 
 template<typename T, typename To = void, bool BoolOtherTest = true>
-using Public = _helper::_access::_enable_if::_Public<BoolOtherTest ,T, To>;
+using Public = _helper::_basic::_type::_access::
+    _enable_if::_Public<BoolOtherTest ,T, To>;
 
 template<typename T, typename To = void, bool BoolOtherTest = true>
-using Protected = _helper::_access::_enable_if::
-    _Protected<BoolOtherTest, T, To>;
-    
+using Protected = _helper::_basic::_type::_access::
+    _enable_if::_Protected<BoolOtherTest, T, To>;
+
 template<typename T, typename To = void, bool BoolOtherTest = true>
-using Private = _helper::_access::_enable_if::
-    _Private<BoolOtherTest, T, To>;
+using Private = _helper::_basic::_type::_access::
+    _enable_if::_Private<BoolOtherTest, T, To>;
 
 } //!enable_if
 
