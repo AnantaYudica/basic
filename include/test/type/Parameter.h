0d0629286cc6b084f0d679643c19131bf6aec135
AnantaYudica <nta040689@gmail.com>
2018-09-11 22:21:04 +0700

    Update basic::test::type::Parameter

--- include/test/type/Parameter.h
+++ include/test/type/Parameter.h
@@ -1,6 +1,10 @@
 #ifndef BASIC_TEST_TYPE_PARAMETER_H_
 #define BASIC_TEST_TYPE_PARAMETER_H_
 
+#include "param/Name.h"
+
+#include <utility>
+
 namespace basic
 {
 namespace test
@@ -9,9 +13,50 @@ namespace type
 {
 
 template<typename... TArgs>
-class Parameter
+struct Parameter
 {
+    using NameType = param::Name<TArgs...>;
+    template<template<typename...> class TT>
+    using FilledType = TT<TArgs...>;
+    template<typename TRet>
+    using PointerFunctionType = TRet(*)(TArgs...);
+    template<typename TRet, typename TDerived>
+    using PointerFunctionMemberType = TRet(TDerived::*)(TArgs...);
+    template<typename TRet, typename TDerived>
+    using PointerFunctionConstMemberType = TRet(TDerived::*)(TArgs...) const;
+    template<typename TRet>
+    static TRet Call(PointerFunctionType<TRet> ptr_func, TArgs&&... args);
+    template<typename TRet, typename TDerived>
+    static TRet Call(PointerFunctionMemberType<TRet, TDerived> ptr_func_mmbr,
+        TDerived& d, TArgs&&... args);
+    template<typename TRet, typename TDerived>
+    static TRet Call(PointerFunctionConstMemberType<TRet, 
+        TDerived> ptr_func_cosnt_mmbr, const TDerived& d, TArgs&&... args);
 };
+
+template<typename... TArgs>
+template<typename TRet>
+TRet Parameter<TArgs...>::Call(PointerFunctionType<TRet> ptr_func, 
+    TArgs&&... args)
+{
+    return ptr_func(std::forward<TArgs>(args)...);
+}
+
+template<typename... TArgs>
+template<typename TRet, typename TDerived>
+TRet Parameter<TArgs...>::Call(PointerFunctionMemberType<TRet, 
+    TDerived> ptr_func_mmbr, TDerived& d, TArgs&&... args)
+{
+    return (d.*ptr_func_mmbr)(std::forward<TArgs>(args)...);
+}
+
+template<typename... TArgs>
+template<typename TRet, typename TDerived>
+TRet Parameter<TArgs...>::Call(PointerFunctionConstMemberType<TRet,
+    TDerived> ptr_func_cosnt_mmbr, const TDerived& d, TArgs&&... args)
+{
+    return (d.*ptr_func_cosnt_mmbr)(std::forward<TArgs>(args)...);
+}
 
 } //!type
 
