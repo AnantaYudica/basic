e6614db4e4f2fb1829f7cb7d741d3ee35e051429
AnantaYudica <nta040689@gmail.com>
2018-09-12 21:12:12 +0700

    Update basic::test::msg::Format

--- include/test/msg/Format.h
+++ include/test/msg/Format.h
@@ -1,10 +1,14 @@
 #ifndef BASIC_TEST_MSG_FORMAT_H_
 #define BASIC_TEST_MSG_FORMAT_H_
 
+#include "../CString.h"
+#include "../cstr/Format.h"
+
 #include <cstddef>
 #include <cstring>
 #include <cstdio>
 #include <utility>
+#include <type_traits>
 
 namespace basic
 {
@@ -13,169 +17,138 @@ namespace test
 namespace msg
 {
 
+template<typename TChar = char>
 class Format
 {
+public:
+    typedef typename std::remove_const<TChar>::type CharType;
+    typedef std::size_t SizeType;
 private:
-    char* m_value;
-    std::size_t m_size;
+    test::CString<CharType> m_cstr;
 public:
     Format();
     template<std::size_t S>
-    Format(const char (&cstr)[S]);
-    Format(const char* cstr, const std::size_t& size);
+    Format(const CharType (&cstr)[S]);
+    Format(const CharType* cstr, const SizeType& size);
+    Format(const test::CString<CharType>& cstr);
+    Format(const test::CString<const CharType>& cstr);
+    Format(test::CString<CharType>&& cstr);
+    Format(test::CString<const CharType>&& cstr);
     template<typename... TArgs>
-    Format(const Format& format, const std::size_t& size,
+    Format(const Format<TChar>& format, const SizeType& size,
         TArgs&&... args);
-    Format(const Format&);
-    Format(Format&&);
+    Format(const Format<TChar>&);
+    Format(Format<TChar>&&);
     ~Format();
 public:
-    Format& operator=(const Format&);
-    Format& operator=(Format&&);
+    Format<TChar>& operator=(const Format<TChar>&);
+    Format<TChar>& operator=(Format<TChar>&&);
 public:
-    const char* Value() const;
-    std::size_t Size() const;
-    bool IsEmpty() const;
     operator bool() const;
+    const TChar* operator*() const;
+    typename Format<TChar>::SizeType Size() const;
+    bool IsEmpty() const;
 };
 
-inline Format::Format() :
-    m_size(0),
-    m_value(nullptr)
+template<typename TChar>
+Format<TChar>::Format()
 {}
 
+template<typename TChar>
 template<std::size_t S>
-inline Format::Format(const char (&cstr)[S]) :
-    m_size(S),
-    m_value(new char[S])
-{
-    memcpy(m_value, cstr, S);
-}
+Format<TChar>::Format(const CharType (&cstr)[S]) :
+    m_cstr(cstr)
+{}
 
-inline Format::Format(const char* cstr, const std::size_t& size) :
-    m_size(size),
-    m_value(new char[size])
-{
-    memcpy(m_value, cstr, size);
-}
+template<typename TChar>
+Format<TChar>::Format(const CharType* cstr, const SizeType& size) :
+    m_cstr(cstr, size)
+{}
 
+template<typename TChar>
+Format<TChar>::Format(const test::CString<CharType>& cstr) :
+    m_cstr(cstr)
+{}
+
+template<typename TChar>
+Format<TChar>::Format(const test::CString<const CharType>& cstr) :
+    m_cstr(cstr)
+{}
+
+template<typename TChar>
+Format<TChar>::Format(test::CString<CharType>&& cstr) :
+    m_cstr(std::move(cstr))
+{}
+
+template<typename TChar>
+Format<TChar>::Format(test::CString<const CharType>&& cstr) :
+    m_cstr(std::move(cstr))
+{}
+
+template<typename TChar>
 template<typename... TArgs>
-inline Format::Format(const Format& format, const std::size_t& size, 
+Format<TChar>::Format(const Format<TChar>& format, const SizeType& size, 
     TArgs&&... args) : 
-        m_size(size),
-        m_value(new char[size])
+        m_cstr(std::move(test::cstr::Format<CharType>(size, *format, 
+            std::forward<TArgs>(args)...)))
+{}
+
+template<typename TChar>
+Format<TChar>::Format(const Format<TChar>& cpy) :
+    m_cstr(cpy.m_cstr)
+{}
+
+template<typename TChar>
+Format<TChar>::Format(Format<TChar>&& mov) :
+    m_cstr(std::move(mov.m_cstr))
+{}
+
+template<typename TChar>
+Format<TChar>::~Format()
+{}
+
+template<typename TChar>
+Format<TChar>& Format<TChar>::operator=(const Format<TChar>& cpy)
 {
-    if (size != 0)
-    {
-        int ret = snprintf(m_value, m_size, format.Value(), 
-            std::forward<TArgs>(args)...);
-        if (ret < 0)
-        {
-            *(m_value) = '\0';
-        }
-        else if(ret >= m_size)
-        {
-            *(m_value + (m_size - 1)) = '\0';
-        }
-    }
+    m_cstr = cpy.m_cstr;
+    return *this;
 }
 
-inline Format::Format(const Format& cpy)
+template<typename TChar>
+Format<TChar>& Format<TChar>::operator=(Format<TChar>&& mov)
 {
-    if (cpy.operator bool())
-    {
-        m_value = new char[cpy.m_size];
-        m_size = cpy.m_size;
-        memcpy(m_value, cpy.m_value, cpy.m_size);
-    }
-    else
-    {
-        m_value = nullptr;
-        m_size = 0;
-    }
-       
+    m_cstr = std::move(mov.m_cstr);
+    return *this;
 }
 
-inline Format::Format(Format&& mov)
+template<typename TChar>
+Format<TChar>::operator bool() const
 {
-    if (mov.operator bool())
-    {
-        m_value = mov.m_value;
-        m_size = mov.m_size;
-        mov.m_value = nullptr;
-        mov.m_size = 0;
-    }
-    else
-    {
-        m_value = nullptr;
-        m_size = 0;
-    }
+    return *m_cstr != nullptr; 
 }
 
-inline Format::~Format()
+template<typename TChar>
+const TChar* Format<TChar>::operator*() const
 {
     if (operator bool())
-    {
-        delete[] m_value;
-        m_value = nullptr;
-        m_size = 0;
-    }
+        return *m_cstr;
+    return "";
 }
 
-inline Format& Format::operator=(const Format& cpy)
+template<typename TChar>
+typename Format<TChar>::SizeType Format<TChar>::Size() const
 {
-    if (operator bool())
-        delete[] m_value;
-    if (cpy.operator bool())
-    {
-        m_value = new char[cpy.m_size];
-        m_size = cpy.m_size;
-        memcpy(m_value, cpy.m_value, cpy.m_size);
-    }
-    else
-        m_size = 0;
+    return m_cstr.Size();
 }
 
-inline Format& Format::operator=(Format&& mov)
+template<typename TChar>
+bool Format<TChar>::IsEmpty() const
 {
-    if (operator bool())
-        delete[] m_value;
-    if (mov.operator bool())
-    {
-        m_value = mov.m_value;
-        m_size = mov.m_size;
-        mov.m_value = nullptr;
-        mov.m_size = 0;
-    }
-    else
-        m_size = 0;
-}
-
-inline const char* Format::Value() const
-{
-    if (operator bool()) 
-        return m_value;
-    return nullptr;
-}
-
-inline std::size_t Format::Size() const
-{
-    if (operator bool()) 
-        return m_size;
-    return 0;
-}
-
-inline bool Format::IsEmpty() const
-{
-    if (operator bool() && m_value[0] == '\0')
+    if (!operator bool() || m_cstr[0] != '\0')
         return false;
     return true;
 }
 
-inline Format::operator bool() const
-{
-    return m_value != nullptr; 
-}
 
 } //!msg
 
