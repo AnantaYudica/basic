1c2c08ea00498e9f558a48476cb1972e36a79091
AnantaYudica <nta040689@gmail.com>
2018-09-21 16:48:07 +0700

    Add basic::test::type::param::Element

---
+++ include/test/type/param/Element.h
@@ -0,0 +1,45 @@
+#ifndef BASIC_TEST_TYPE_PARAM_ELEMENT_H_
+#define BASIC_TEST_TYPE_PARAM_ELEMENT_H_
+
+#include "../Parameter.h"
+
+#include <cstddef>
+
+namespace basic
+{
+namespace test
+{
+namespace type
+{
+namespace param
+{
+
+template<std::size_t I, typename T>
+struct Element
+{};
+
+template<typename TArg, typename... TArgs>
+struct Element<0, type::Parameter<TArg, TArgs...>>
+{
+    typedef TArg type;
+    typedef TArg Type;
+};
+
+template<std::size_t I, typename TArg, typename... TArgs>
+struct Element<I, type::Parameter<TArg, TArgs...>>
+{
+    typedef typename Element<I - 1, test::type::
+        Parameter<TArgs...>>::type type;
+    typedef typename Element<I - 1, test::type::
+        Parameter<TArgs...>>::Type Type;
+};
+
+} //!param
+
+} //!type
+
+} //!test
+
+} //!basic
+
+#endif //!BASIC_TEST_TYPE_PARAM_ELEMENT_H_
