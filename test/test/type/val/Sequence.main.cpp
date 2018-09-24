5d14cf95650e327e1cc0ab19de421645afbd135d
AnantaYudica <nta040689@gmail.com>
2018-09-24 11:46:52 +0700

    Update test experimental for test/type/val/Sequence.h

--- test/test/type/val/Sequence.main.cpp
+++ test/test/type/val/Sequence.main.cpp
@@ -13,13 +13,13 @@ int main()
     printf("Size : %d\n", ValSeqInt0::Size);
     typedef basic::test::type::val::Sequence<int, 4> ValSeqInt1;
     ValSeqInt1 i1;
-    printf("[0] : %d\n", ValSeqInt1::At<0>());
+    printf("[0] : %d\n", ValSeqInt1{}.At<0>());
     printf("Size : %d\n", ValSeqInt1::Size);
     typedef basic::test::type::val::Sequence<int, 4, 6, 11, 14> ValSeqInt4;
     ValSeqInt4 i4;
-    printf("[0] : %d\n", ValSeqInt4::At<0>());
-    printf("[1] : %d\n", ValSeqInt4::At<1>());
-    printf("[2] : %d\n", ValSeqInt4::At<2>());
-    printf("[3] : %d\n", ValSeqInt4::At<3>());
+    printf("[0] : %d\n", ValSeqInt4{}.At<0>());
+    printf("[1] : %d\n", ValSeqInt4{}.At<1>());
+    printf("[2] : %d\n", ValSeqInt4{}.At<2>());
+    printf("[3] : %d\n", ValSeqInt4{}.At<3>());
     printf("Size : %d\n", ValSeqInt4::Size);
 }
