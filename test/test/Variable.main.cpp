9cd1d5c16d83dd5694a9175f4c2577e4457f15cd
AnantaYudica <nta040689@gmail.com>
2018-09-24 08:55:01 +0700

    Update test experimental for test/Variable.h

--- test/test/Variable.main.cpp
+++ test/test/Variable.main.cpp
@@ -7,5 +7,5 @@ int main()
     basic::test::Variable<> var1;
     basic::test::Variable<int> var2;
     basic::test::Variable<int, char> var3;
-    basic::test::Variable<int, char, float> var3;
+    basic::test::Variable<int, char, float> var4;
 }
