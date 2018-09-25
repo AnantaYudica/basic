8554eda884f492e38c7a943db44ae43eaa702e3d
AnantaYudica <nta040689@gmail.com>
2018-09-25 15:06:07 +0700

    Update basic::test::Parameter

--- include/test/Parameter.h
+++ include/test/Parameter.h
@@ -11,17 +11,17 @@ namespace basic
 {
 namespace test
 {
-    
-template<typename TTypeParam>
+
+template<typename... TArgs>
 class Parameter
 {
 public:
     Parameter();
-    Parameter(const Parameter<TTypeParam>& cpy);
-    Parameter(Parameter<TTypeParam>&& mov);
+    Parameter(const Parameter<TArgs...>& cpy);
+    Parameter(Parameter<TArgs...>&& mov);
 public:
-    Parameter<TTypeParam>& operator=(const Parameter<TTypeParam>& cpy);
-    Parameter<TTypeParam>& operator=(Parameter<TTypeParam>&& mov);
+    Parameter<TArgs...>& operator=(const Parameter<TArgs...>& cpy);
+    Parameter<TArgs...>& operator=(Parameter<TArgs...>&& mov);
 protected:
     template<typename TRet, typename TFunc, typename... TValues>
     TRet FillerFunc(TFunc func, TValues&&... values);
@@ -47,20 +47,20 @@ public:
 };
 
 template<typename TArg, typename... TArgs>
-class Parameter<test::type::Parameter<TArg, TArgs...>> :
-    Parameter<test::type::Parameter<TArgs...>>
+class Parameter<TArg, TArgs...> :
+    Parameter<TArgs...>
 {
 private:
     TArg m_value;
 public:
     Parameter(const TArg& val, const TArgs&... vals);
-    Parameter(const Parameter<test::type::Parameter<TArg, TArgs...>>& cpy);
-    Parameter(Parameter<test::type::Parameter<TArg, TArgs...>>&& mov);
+    Parameter(const Parameter<TArg, TArgs...>& cpy);
+    Parameter(Parameter<TArg, TArgs...>&& mov);
 public:
-    Parameter<test::type::Parameter<TArg, TArgs...>>& 
-        operator=(const Parameter<test::type::Parameter<TArg, TArgs...>>& cpy);
-    Parameter<test::type::Parameter<TArg, TArgs...>>& 
-        operator=(Parameter<test::type::Parameter<TArg, TArgs...>>&& mov);
+    Parameter<TArg, TArgs...>& 
+        operator=(const Parameter<TArg, TArgs...>& cpy);
+    Parameter<TArg, TArgs...>& 
+        operator=(Parameter<TArg, TArgs...>&& mov);
 protected:
     template<typename TRet, typename TFunc, typename... TValues>
     TRet FillerFunc(TFunc func, TValues&&... values);
@@ -94,134 +94,134 @@ public:
             At() const;
 };
 
-template<typename TTypeParam>
-Parameter<TTypeParam>::Parameter()
+template<typename... TArgs>
+Parameter<TArgs...>::Parameter()
 {}
 
-template<typename TTypeParam>
-Parameter<TTypeParam>::Parameter(const Parameter<TTypeParam>& cpy)
+template<typename... TArgs>
+Parameter<TArgs...>::Parameter(const Parameter<TArgs...>& cpy)
 {}
 
-template<typename TTypeParam>
-Parameter<TTypeParam>::Parameter(Parameter<TTypeParam>&& mov)
+template<typename... TArgs>
+Parameter<TArgs...>::Parameter(Parameter<TArgs...>&& mov)
 {}
 
-template<typename TTypeParam>
-Parameter<TTypeParam>& Parameter<TTypeParam>::
-    operator=(const Parameter<TTypeParam>& cpy)
+template<typename... TArgs>
+Parameter<TArgs...>& Parameter<TArgs...>::
+    operator=(const Parameter<TArgs...>& cpy)
 {
     return *this;
 }
 
-template<typename TTypeParam>
-Parameter<TTypeParam>& Parameter<TTypeParam>::
-    operator=(Parameter<TTypeParam>&& mov)
+template<typename... TArgs>
+Parameter<TArgs...>& Parameter<TArgs...>::
+    operator=(Parameter<TArgs...>&& mov)
 {
     return *this;
 }
 
-template<typename TTypeParam>
+template<typename... TArgs>
 template<typename TRet, typename TFunc, typename... TValues>
-TRet Parameter<TTypeParam>::FillerFunc(TFunc func, TValues&&... values)
+TRet Parameter<TArgs...>::FillerFunc(TFunc func, TValues&&... values)
 {
     return func(std::forward<TValues>(values)...);
 }
 
-template<typename TTypeParam>
+template<typename... TArgs>
 template<typename TRet, typename TDerived, typename TFuncMmbr, 
     typename... TValues>
 typename std::enable_if<std::is_pointer<TDerived>::value, TRet>::type 
-    Parameter<TTypeParam>::FillerFuncMmbr(TFuncMmbr func_mmbr, TDerived derived, 
+    Parameter<TArgs...>::FillerFuncMmbr(TFuncMmbr func_mmbr, TDerived derived, 
         TValues&&... values)
 {
     return (derived->*func_mmbr)(std::forward<TValues>(values)...);
 }
 
-template<typename TTypeParam>
+template<typename... TArgs>
 template<typename TRet, typename TDerived, typename TFuncMmbr, 
     typename... TValues>
 typename std::enable_if<!std::is_pointer<TDerived>::value, TRet>::type 
-    Parameter<TTypeParam>::FillerFuncMmbr(TFuncMmbr func_mmbr, TDerived derived, 
+    Parameter<TArgs...>::FillerFuncMmbr(TFuncMmbr func_mmbr, TDerived derived, 
         TValues&&... values)
 {
     return (derived.*func_mmbr)(std::forward<TValues>(values)...);
 }
 
-template<typename TTypeParam>
+template<typename... TArgs>
 template<typename TRet, typename... TValues>
-TRet&& Parameter<TTypeParam>::FillerConstruct(TValues&&... values)
+TRet&& Parameter<TArgs...>::FillerConstruct(TValues&&... values)
 {
     return std::move(TRet{std::forward<TValues>(values)...});
 }
 
-template<typename TTypeParam>
+template<typename... TArgs>
 template<typename TRet, typename TFunc>
-TRet Parameter<TTypeParam>::Fill(TFunc func)
+TRet Parameter<TArgs...>::Fill(TFunc func)
 {
     return FillerFunc<TRet>(func);
 }
 
-template<typename TTypeParam>
+template<typename... TArgs>
 template<typename TRet, typename TDerived, typename TFuncMmbr>
-TRet Parameter<TTypeParam>::Fill(TFuncMmbr func_mmbr, TDerived derived)
+TRet Parameter<TArgs...>::Fill(TFuncMmbr func_mmbr, TDerived derived)
 {
     return FillerFuncMmbr<TRet>(func_mmbr, derived);
 }
 
-template<typename TTypeParam>
+template<typename... TArgs>
 template<typename TDerived>
-TDerived&& Parameter<TTypeParam>::Fill()
+TDerived&& Parameter<TArgs...>::Fill()
 {
     return std::move(FillerConstruct<TDerived>());
 }
 
 template<typename TArg, typename... TArgs>
-Parameter<test::type::Parameter<TArg, TArgs...>>::
+Parameter<TArg, TArgs...>::
     Parameter(const TArg& val, const TArgs&... vals) :
-        Parameter<test::type::Parameter<TArgs...>>(vals...),
+        Parameter<TArgs...>(vals...),
         m_value(val)
 {}
 
 template<typename TArg, typename... TArgs>
-Parameter<test::type::Parameter<TArg, TArgs...>>::
-    Parameter(const Parameter<test::type::Parameter<TArg, TArgs...>>& cpy) :
-        Parameter<test::type::Parameter<TArgs...>>(cpy),
+Parameter<TArg, TArgs...>::
+    Parameter(const Parameter<TArg, TArgs...>& cpy) :
+        Parameter<TArgs...>(cpy),
         m_value(cpy.m_value)
 {}
 
 template<typename TArg, typename... TArgs>
-Parameter<test::type::Parameter<TArg, TArgs...>>::
-    Parameter(Parameter<test::type::Parameter<TArg, TArgs...>>&& mov) :
-        Parameter<test::type::Parameter<TArgs...>>(std::move(mov)),
+Parameter<TArg, TArgs...>::
+    Parameter(Parameter<TArg, TArgs...>&& mov) :
+        Parameter<TArgs...>(std::move(mov)),
         m_value(std::move(mov.m_value))
 {}
 
 template<typename TArg, typename... TArgs>
-Parameter<test::type::Parameter<TArg, TArgs...>>& 
-    Parameter<test::type::Parameter<TArg, TArgs...>>::
-        operator=(const Parameter<test::type::Parameter<TArg, TArgs...>>& cpy)
+Parameter<TArg, TArgs...>& 
+    Parameter<TArg, TArgs...>::
+        operator=(const Parameter<TArg, TArgs...>& cpy)
 {
-    Parameter<test::type::Parameter<TArgs...>>::operator=(cpy);
+    Parameter<TArgs...>::operator=(cpy);
     m_value = cpy.m_value;
     return *this;
 }
 
 template<typename TArg, typename... TArgs>
-Parameter<test::type::Parameter<TArg, TArgs...>>& 
-    Parameter<test::type::Parameter<TArg, TArgs...>>::
-        operator=(Parameter<test::type::Parameter<TArg, TArgs...>>&& mov)
+Parameter<TArg, TArgs...>& 
+    Parameter<TArg, TArgs...>::
+        operator=(Parameter<TArg, TArgs...>&& mov)
 {
-    Parameter<test::type::Parameter<TArgs...>>::operator=(std::move(mov));
+    Parameter<TArgs...>::operator=(std::move(mov));
     m_value = std::move(mov.m_value);
     return *this;
 }
 
 template<typename TArg, typename... TArgs>
 template<typename TRet, typename TFunc, typename... TValues>
-TRet Parameter<test::type::Parameter<TArg, TArgs...>>::
+TRet Parameter<TArg, TArgs...>::
     FillerFunc(TFunc func, TValues&&... values)
 {
-    return Parameter<test::type::Parameter<TArgs...>>::
+    return Parameter<TArgs...>::
         template FillerFunc<TRet>(func, std::forward<TValues>(values)...,
             std::forward<TArg>(m_value));
 }
@@ -229,34 +229,34 @@ TRet Parameter<test::type::Parameter<TArg, TArgs...>>::
 template<typename TArg, typename... TArgs>
 template<typename TRet, typename TDerived, typename TFuncMmbr, 
     typename... TValues>
-TRet Parameter<test::type::Parameter<TArg, TArgs...>>::
+TRet Parameter<TArg, TArgs...>::
     FillerFuncMmbr(TFuncMmbr func_mmbr, TDerived derived, TValues&&... values)
 {
-    return Parameter<test::type::Parameter<TArgs...>>::
+    return Parameter<TArgs...>::
         template FillerFuncMmbr<TRet>(func_mmbr, derived, 
             std::forward<TValues>(values)..., std::forward<TArg>(m_value));
 }
 
 template<typename TArg, typename... TArgs>
 template<typename TRet, typename... TValues>
-TRet&& Parameter<test::type::Parameter<TArg, TArgs...>>::
+TRet&& Parameter<TArg, TArgs...>::
     FillerConstruct(TValues&&... values)
 {
-    return std::move(Parameter<test::type::Parameter<TArgs...>>::
+    return std::move(Parameter<TArgs...>::
         template FillerConstruct<TRet>(std::forward<TValues>(values)..., 
             std::forward<TArg>(m_value)));
 }
 
 template<typename TArg, typename... TArgs>
 template<typename TRet, typename TFunc>
-TRet Parameter<test::type::Parameter<TArg, TArgs...>>::Fill(TFunc func)
+TRet Parameter<TArg, TArgs...>::Fill(TFunc func)
 {
     return FillerFunc<TRet>(func);
 }
 
 template<typename TArg, typename... TArgs>
 template<typename TRet, typename TDerived, typename TFuncMmbr>
-TRet Parameter<test::type::Parameter<TArg, TArgs...>>::
+TRet Parameter<TArg, TArgs...>::
     Fill(TFuncMmbr func_mmbr, TDerived derived)
 {
     return FillerFuncMmbr<TRet>(func_mmbr, derived);
@@ -264,7 +264,7 @@ TRet Parameter<test::type::Parameter<TArg, TArgs...>>::
 
 template<typename TArg, typename... TArgs>
 template<typename TDerived>
-TDerived&& Parameter<test::type::Parameter<TArg, TArgs...>>::Fill()
+TDerived&& Parameter<TArg, TArgs...>::Fill()
 {
     return std::move(FillerConstruct<TDerived>());
 }
@@ -273,7 +273,7 @@ template<typename TArg, typename... TArgs>
 template<std::size_t I>
 typename std::enable_if<I == 0, typename test::type::param::Element<I, 
     test::type::Parameter<TArg, TArgs...>>::Type>::type& 
-        Parameter<test::type::Parameter<TArg, TArgs...>>::At()
+        Parameter<TArg, TArgs...>::At()
 {
     return m_value;
 }
@@ -282,9 +282,9 @@ template<typename TArg, typename... TArgs>
 template<std::size_t I>
 typename std::enable_if<I != 0, typename test::type::param::Element<I, 
     test::type::Parameter<TArg, TArgs...>>::Type>::type& 
-        Parameter<test::type::Parameter<TArg, TArgs...>>::At()
+        Parameter<TArg, TArgs...>::At()
 {
-    return Parameter<test::type::Parameter<TArgs...>>::
+    return Parameter<TArgs...>::
         template At<I - 1>();
 }
 
@@ -292,7 +292,7 @@ template<typename TArg, typename... TArgs>
 template<std::size_t I>
 const typename std::enable_if<I == 0, typename test::type::param::
     Element<I,test::type::Parameter<TArg, TArgs...>>::Type>::type& 
-        Parameter<test::type::Parameter<TArg, TArgs...>>::At() const
+        Parameter<TArg, TArgs...>::At() const
 {
     return m_value;
 }
@@ -301,9 +301,9 @@ template<typename TArg, typename... TArgs>
 template<std::size_t I>
 const typename std::enable_if<I != 0, typename test::type::param::
     Element<I,test::type::Parameter<TArg, TArgs...>>::Type>::type& 
-        Parameter<test::type::Parameter<TArg, TArgs...>>::At() const
+        Parameter<TArg, TArgs...>::At() const
 {
-    return Parameter<test::type::Parameter<TArgs...>>::
+    return Parameter<TArgs...>::
         template At<I - 1>();;
 }
 
