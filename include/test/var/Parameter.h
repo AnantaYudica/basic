42b3123f1f16da4bf560b29c2e77f153b6c4d216
AnantaYudica <nta040689@gmail.com>
2018-09-25 15:07:00 +0700

    Update basic::test::var::Parameter

--- include/test/var/Parameter.h
+++ include/test/var/Parameter.h
@@ -3,7 +3,6 @@
 
 #include "../Variable.h"
 
-#include "../type/Parameter.h"
 #include "../Parameter.h"
 
 namespace basic
@@ -12,30 +11,29 @@ namespace test
 {
 
 template<typename... TValuesArg, typename... TArgs>
-class Variable<test::Parameter<test::type::
-    Parameter<TValuesArg...>>, TArgs...> :
+class Variable<test::Parameter<TValuesArg...>, TArgs...> :
         public Variable<TArgs...>
 {
 public:
-    typedef test::Parameter<test::type::Parameter<TValuesArg...>>& GetType;
+    typedef test::Parameter<TValuesArg...>& GetType;
     typedef const test::Parameter<test::type::
         Parameter<TValuesArg...>>& ConstGetType;
 private:
-    test::Parameter<test::type::Parameter<TValuesArg...>> m_value;
+    test::Parameter<TValuesArg...> m_value;
 public:
     template<typename... TValArgs>
     Variable(TValuesArg&&... vals_arg, TValArgs&&... val_args);
     Variable(const Variable<test::Parameter<
-        test::type::Parameter<TValuesArg...>>, TArgs...>& cpy) = delete;
+        TValuesArg...>, TArgs...>& cpy) = delete;
     Variable(Variable<test::Parameter<
-        test::type::Parameter<TValuesArg...>>, TArgs...>&&) = delete;
+        TValuesArg...>, TArgs...>&&) = delete;
 public:
-    Variable<test::Parameter<test::type::Parameter<TValuesArg...>>, 
+    Variable<test::Parameter<TValuesArg...>, 
         TArgs...>& operator=(const Variable<test::Parameter<
-            test::type::Parameter<TValuesArg...>>, TArgs...>& cpy);
-    Variable<test::Parameter<test::type::Parameter<TValuesArg...>>, 
+            TValuesArg...>, TArgs...>& cpy);
+    Variable<test::Parameter<TValuesArg...>, 
         TArgs...>& operator=(Variable<test::Parameter<
-            test::type::Parameter<TValuesArg...>>, TArgs...>&&) = delete;
+            TValuesArg...>, TArgs...>&&) = delete;
 public:
     GetType Get();
     ConstGetType Get() const;
@@ -43,17 +41,17 @@ public:
 
 template<typename... TValuesArg, typename... TArgs>
 template<typename... TValArgs>
-Variable<test::Parameter<test::type::Parameter<TValuesArg...>>, TArgs...>::
+Variable<test::Parameter<TValuesArg...>, TArgs...>::
     Variable(TValuesArg&&... vals_arg, TValArgs&&... val_args) :
         Variable<TArgs...>(std::forward<TValArgs>(val_args)...),
         m_value(std::forward<TValuesArg>(vals_arg)...)
 {}
 
 template<typename... TValuesArg, typename... TArgs>
-Variable<test::Parameter<test::type::Parameter<TValuesArg...>>, TArgs...>& 
-    Variable<test::Parameter<test::type::Parameter<TValuesArg...>>, TArgs...>::
+Variable<test::Parameter<TValuesArg...>, TArgs...>& 
+    Variable<test::Parameter<TValuesArg...>, TArgs...>::
         operator=(const Variable<test::Parameter<
-            test::type::Parameter<TValuesArg...>>, TArgs...>& cpy)
+            TValuesArg...>, TArgs...>& cpy)
 {
     Variable<TArgs...>::operator=(cpy);
     m_value = cpy.m_value;
@@ -61,17 +59,17 @@ Variable<test::Parameter<test::type::Parameter<TValuesArg...>>, TArgs...>&
 }
 
 template<typename... TValuesArg, typename... TArgs>
-typename Variable<test::Parameter<test::type::Parameter<TValuesArg...>>, 
-    TArgs...>::GetType Variable<test::Parameter<test::type::
-        Parameter<TValuesArg...>>, TArgs...>::Get()
+typename Variable<test::Parameter<TValuesArg...>, 
+    TArgs...>::GetType Variable<test::Parameter<TValuesArg...>, 
+        TArgs...>::Get()
 {
     return m_value;
 }
 
 template<typename... TValuesArg, typename... TArgs>
-typename Variable<test::Parameter<test::type::Parameter<TValuesArg...>>,
-    TArgs...>::ConstGetType Variable<test::Parameter<test::type::
-        Parameter<TValuesArg...>>, TArgs...>::Get() const
+typename Variable<test::Parameter<TValuesArg...>,
+    TArgs...>::ConstGetType Variable<test::Parameter<TValuesArg...>, 
+        TArgs...>::Get() const
 {
     return m_value;
 }
