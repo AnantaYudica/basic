ab701a0e0d0716fefb7b471de36f23bf86c793dc
AnantaYudica <nta040689@gmail.com>
2018-09-25 18:28:04 +0700

    Update basic::test::var::Value

--- include/test/var/Value.h
+++ include/test/var/Value.h
@@ -62,36 +62,36 @@ public:
 #define REMOVED_DEPRECATED
 #endif //!REMOVED_DEPRECATED
     template<std::size_t I, typename TDefArg, typename... TDefArgs>
-    struct Definition
+    struct _Definition
     {
-        typedef typename Definition<I - 1, TDefArgs...>::ValueType ValueType;
-        typedef typename Definition<I - 1, TDefArgs...>::Type Type;
+        typedef typename _Definition<I - 1, TDefArgs...>::ValueType ValueType;
+        typedef typename _Definition<I - 1, TDefArgs...>::Type Type;
     };
     template<typename TDefArg, typename... TDefArgs>
-    struct Definition<0, TDefArg, TDefArgs...>
+    struct _Definition<0, TDefArg, TDefArgs...>
     {
         typedef typename TDefArg::Type ValueType;
         typedef TDefArg Type;
     };
     template<typename TVar>
-    static constexpr auto LValue(int) -> decltype(
+    static constexpr auto _LValue(int) -> decltype(
         static_cast<typename TVar::GetType(TVar::*)()>(&TVar::Get), 
             std::true_type());
     template<typename TVar>
-    static constexpr std::false_type LValue(...);
+    static constexpr std::false_type _LValue(...);
 public:
     template<std::size_t I>
-    typename std::enable_if<decltype(LValue<typename Definition<I, test::Value<TArg>, 
+    typename std::enable_if<decltype(_LValue<typename _Definition<I, test::Value<TArg>, 
         TArgs...>::Type>(0))::value
-        && I != 0, typename Definition<I, test::Value<TArg>, TArgs...>::ValueType>::
+        && I != 0, typename _Definition<I, test::Value<TArg>, TArgs...>::ValueType>::
         type GetValue()
     {
         return Variable<TArgs...>::template GetValue<I - 1>();
     }
     template<std::size_t I>
-    typename std::enable_if<decltype(LValue<typename Definition<I, test::Value<TArg>, 
+    typename std::enable_if<decltype(_LValue<typename _Definition<I, test::Value<TArg>, 
         TArgs...>::Type>(0))::value
-        && I == 0, typename Definition<I, test::Value<TArg>, TArgs...>::ValueType>::
+        && I == 0, typename _Definition<I, test::Value<TArg>, TArgs...>::ValueType>::
         type GetValue()
     {
         return m_value.Get();
