67d4edaf20893b14ab4ba22624787a623ebda8cb
AnantaYudica <nta040689@gmail.com>
2018-09-27 17:24:54 +0700

    Update basic::test:msg::arg::type::param::Name

--- include/test/msg/arg/type/param/Name.h
+++ include/test/msg/arg/type/param/Name.h
@@ -7,7 +7,7 @@
 #include "../../../../var/Element.h"
 #include "../../../../type/Parameter.h"
 #include "../../../../type/param/Name.h"
-
+#include "../../../../CString.h"
 #include <cstddef>
 
 namespace basic
@@ -38,19 +38,25 @@ class Argument<TCaseId, arg::type::param::Name<I>, TArgs...> :
 {
 public:
     template<typename TVar>
-    using CharType = typename decltype(type::param::Name<
-        typename Argument<TCaseId>::template ElementType<I, TVar>>::CStr())::
-        CharType;
+    using CharType = typename std::remove_const<typename decltype(type::param::
+        Name<typename Argument<TCaseId>::template ElementType<I, TVar>>::
+        CStr())::CharType>::type;
+public:
+    template<typename TVar>
+    using ValueType = test::CString<CharType<TVar>>;
+public:
+    template<typename TVar>
+    using GetType = ValueType<TVar>;
 public:
     template<typename TRet, typename TDerived, typename TVar, 
         typename... TFuncMmbrArgs>
     using PointerFunctionMemberType = typename Argument<TCaseId, TArgs...>::
         template PointerFunctionMemberType<TRet, TDerived, TVar, 
-        TFuncMmbrArgs..., const CharType<TVar>*&&>;
+        TFuncMmbrArgs..., GetType<TVar>&&>;
     template<typename TRet, typename TVar, typename... TFuncArgs>
     using PointerFunctionType = typename Argument<TCaseId, TArgs...>::
         template PointerFunctionType<TRet, TVar, TFuncArgs...,
-        const CharType<TVar>*&&>;
+        GetType<TVar>&&>;
 public:
     Argument();
 protected:
@@ -72,8 +78,11 @@ public:
     TRet Call(PointerFunctionType<TRet, test::Variable<TVarArgs...>, 
         TFuncArgs...> func, test::Variable<TVarArgs...>& var, 
         TFuncArgs&&... args);
+public:
+    template<typename... TVarArgs>
+    GetType<test::Variable<TVarArgs...>> 
+        Get(test::Variable<TVarArgs...>& var) const;
 };
-
 
 template<typename TCaseId, std::size_t I, typename... TArgs>
 Argument<TCaseId, arg::type::param::Name<I>, TArgs...>::Argument()
@@ -86,11 +95,8 @@ TRet Argument<TCaseId, arg::type::param::Name<I>, TArgs...>::
     Filler(TFuncMmbr func_mmbr, TDerived& d,
         test::Variable<TVarArgs...>& var, TFuncMmbrArgs&&... args)
 {
-    test::CString<CharType<test::Variable<TVarArgs...>>> cstr = 
-        std::move(type::param::Name<typename Argument<TCaseId>::
-            template ElementType<I, test::Variable<TVarArgs...>>>::CStr());
     return Argument<TCaseId, TArgs...>:: template Filler<TRet>(func_mmbr, d, 
-        var, std::forward<TFuncMmbrArgs>(args)..., *cstr);
+        var, std::forward<TFuncMmbrArgs>(args)..., std::move(Get(var)));
 }
 
 template<typename TCaseId, std::size_t I, typename... TArgs>
@@ -99,11 +105,8 @@ template<typename TRet, typename TFunc, typename... TFuncArgs,
 TRet Argument<TCaseId, arg::type::param::Name<I>, TArgs...>::
     Filler(TFunc func, test::Variable<TVarArgs...>& var, TFuncArgs&&... args)
 {
-    test::CString<CharType<test::Variable<TVarArgs...>>> cstr = 
-        std::move(type::param::Name<typename Argument<TCaseId>::
-            template ElementType<I, test::Variable<TVarArgs...>>>::CStr());
     return Argument<TCaseId, TArgs...>:: template Filler<TRet>(func, var, 
-        std::forward<TFuncArgs>(args)..., *cstr);
+        std::forward<TFuncArgs>(args)..., std::move(Get(var)));
 }
 
 template<typename TCaseId, std::size_t I, typename... TArgs>
@@ -126,6 +129,17 @@ TRet Argument<TCaseId, arg::type::param::Name<I>, TArgs...>::
         TFuncArgs&&... args)
 {
     return Filler<TRet>(func, var, std::forward<TFuncArgs>(args)...);
+}
+
+template<typename TCaseId, std::size_t I, typename... TArgs>
+template<typename... TVarArgs>
+typename Argument<TCaseId, arg::type::param::Name<I>, TArgs...>::
+    template GetType<test::Variable<TVarArgs...>> Argument<TCaseId, 
+        arg::type::param::Name<I>, TArgs...>::
+            Get(test::Variable<TVarArgs...>& var) const
+{
+    return std::move(type::param::Name<typename Argument<TCaseId>::
+        template ElementType<I, test::Variable<TVarArgs...>>>::CStr());
 }
 
 } //!msg
