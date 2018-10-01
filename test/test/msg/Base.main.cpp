eb24c28135940fb90f8bbb2e480c9138fa886bab
AnantaYudica <nta040689@gmail.com>
2018-10-02 00:48:59 +0700

    Remove dependency with test/msg/arg/var/Value.h

--- test/test/msg/Base.main.cpp
+++ test/test/msg/Base.main.cpp
@@ -7,7 +7,6 @@ BASIC_TEST_CONSTRUCT;
 #include "test/msg/arg/type/Name.h"
 #include "test/msg/arg/type/Value.h"
 #include "test/msg/arg/type/param/Name.h"
-#include "test/msg/arg/var/Value.h"
 #include "test/msg/Base.h"
 
 #include "test/cstr/out/Argument.h"
