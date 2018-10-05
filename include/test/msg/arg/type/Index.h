9452d6c6387d71cecfaede61f5aff400115eb4e6
AnantaYudica <nta040689@gmail.com>
2018-10-06 04:27:53 +0700

    Fix member functions Call and Get from class Basic::test::msg::Argument cause error C2672, C2770 in MSVC

--- include/test/msg/arg/type/Index.h
+++ include/test/msg/arg/type/Index.h
@@ -64,16 +64,41 @@ protected:
     TRet Filler(TFunc func, test::Variable<TVarArgs...>& var, 
         TFuncArgs&&... args);
 public:
+#ifdef _WIN32
+    template<typename TRet, typename TDerived, typename... TFuncMmbrArgs,
+        typename... TVarArgs, typename TFuncMmbr = PointerFunctionMemberType<0,
+        TRet, TDerived, test::Variable<TVarArgs...>, TFuncMmbrArgs...>>
+    TRet Call(TFuncMmbr func_mmbr, TDerived& d,
+        test::Variable<TVarArgs...>& var, TFuncMmbrArgs&&... args);
+#else
     template<typename TRet, typename TDerived, typename... TFuncMmbrArgs,
         typename... TVarArgs>
     TRet Call(PointerFunctionMemberType<0, TRet, TDerived, 
         test::Variable<TVarArgs...>, TFuncMmbrArgs...> func_mmbr, TDerived& d,
         test::Variable<TVarArgs...>& var, TFuncMmbrArgs&&... args);
+#endif
+#ifdef _WIN32
+    template<typename TRet, typename... TFuncArgs, typename... TVarArgs,
+        typename TFunc = PointerFunctionType<0, TRet, 
+        test::Variable<TVarArgs...>, TFuncArgs...>>
+    TRet Call(TFunc func, test::Variable<TVarArgs...>& var,
+        TFuncArgs&&... args);
+#else
     template<typename TRet, typename... TFuncArgs, typename... TVarArgs>
     TRet Call(PointerFunctionType<0, TRet, test::Variable<TVarArgs...>, 
         TFuncArgs...> func, test::Variable<TVarArgs...>& var, 
         TFuncArgs&&... args);
+#endif
 public:
+#ifdef _WIN32
+    template<typename TRet, typename TDerived, typename... TFuncMmbrArgs, 
+        std::size_t ICaseId, typename... TVarArgs, typename TFuncMmbr = 
+        PointerFunctionMemberType<ICaseId, TRet, TDerived,
+        test::Variable<TVarArgs...>, TFuncMmbrArgs...>>
+    TRet Call(const type::Index<TCaseId, ICaseId>&, TFuncMmbr func_mmbr,
+        TDerived& d, test::Variable<TVarArgs...>& var, 
+        TFuncMmbrArgs&&... args);
+#else
     template<typename TRet, typename TDerived, typename... TFuncMmbrArgs, 
         std::size_t ICaseId, typename... TVarArgs>
     TRet Call(const type::Index<TCaseId, ICaseId>&, 
@@ -81,15 +106,31 @@ public:
             test::Variable<TVarArgs...>, TFuncMmbrArgs...> func_mmbr, 
         TDerived& d, test::Variable<TVarArgs...>& var, 
         TFuncMmbrArgs&&... args);
+#endif
+#ifdef _WIN32
+    template<typename TRet, typename... TFuncArgs, std::size_t ICaseId, 
+        typename... TVarArgs, typename TFunc = PointerFunctionType<ICaseId,
+        TRet, test::Variable<TVarArgs...>, TFuncArgs...>>
+    TRet Call(const type::Index<TCaseId, ICaseId>&, TFunc func,
+        test::Variable<TVarArgs...>& var, TFuncArgs&&... args);
+#else
     template<typename TRet, typename... TFuncArgs, std::size_t ICaseId, 
         typename... TVarArgs>
     TRet Call(const type::Index<TCaseId, ICaseId>&, PointerFunctionType<ICaseId, 
         TRet, test::Variable<TVarArgs...>, TFuncArgs...> func, 
         test::Variable<TVarArgs...>& var, TFuncArgs&&... args);
+#endif
 public:
+#ifdef _WIN32
+    template<std::size_t ICaseId, typename... TVarArgs, typename TGet = 
+        typename Argument<TCaseId, arg::type::Index<I, TTArgArg>, TArgs...>::
+        template GetType<ICaseId, test::Variable<TVarArgs...>>>
+    TGet Get(test::Variable<TVarArgs...>& var);
+#else
     template<std::size_t ICaseId, typename... TVarArgs>
     GetType<ICaseId, test::Variable<TVarArgs...>> 
         Get(test::Variable<TVarArgs...>& var);
+#endif
 };
 
 template<typename TCaseId, std::size_t I, template<std::size_t, 
@@ -123,12 +164,20 @@ TRet Argument<TCaseId, arg::type::Index<I, TTArgArg>, TArgs...>::
 
 template<typename TCaseId, std::size_t I, template<std::size_t, 
     std::size_t> class TTArgArg, typename... TArgs>
+#ifdef _WIN32
+template<typename TRet, typename TDerived, typename... TFuncMmbrArgs,
+    typename... TVarArgs, typename TFuncMmbr>
+TRet Argument<TCaseId, arg::type::Index<I, TTArgArg>, TArgs...>::
+    Call(TFuncMmbr func_mmbr, TDerived& d,
+        test::Variable<TVarArgs...>& var, TFuncMmbrArgs&&... args)
+#else
 template<typename TRet, typename TDerived, typename... TFuncMmbrArgs,
     typename... TVarArgs>
 TRet Argument<TCaseId, arg::type::Index<I, TTArgArg>, TArgs...>::
     Call(PointerFunctionMemberType<0, TRet, TDerived, 
         test::Variable<TVarArgs...>, TFuncMmbrArgs...> func_mmbr, TDerived& d,
         test::Variable<TVarArgs...>& var, TFuncMmbrArgs&&... args)
+#endif
 {
     return Filler<0, TRet>(func_mmbr, d, var, 
         std::forward<TFuncMmbrArgs>(args)...);
@@ -136,17 +185,32 @@ TRet Argument<TCaseId, arg::type::Index<I, TTArgArg>, TArgs...>::
 
 template<typename TCaseId, std::size_t I, template<std::size_t, 
     std::size_t> class TTArgArg, typename... TArgs>
+#ifdef _WIN32
+template<typename TRet, typename... TFuncArgs, typename... TVarArgs,
+    typename TFunc>
+TRet Argument<TCaseId, arg::type::Index<I, TTArgArg>, TArgs...>::
+    Call(TFunc func, test::Variable<TVarArgs...>& var,
+        TFuncArgs&&... args)
+#else
 template<typename TRet, typename... TFuncArgs, typename... TVarArgs>
 TRet Argument<TCaseId, arg::type::Index<I, TTArgArg>, TArgs...>::
     Call(PointerFunctionType<0, TRet, test::Variable<TVarArgs...>, 
         TFuncArgs...> func, test::Variable<TVarArgs...>& var, 
         TFuncArgs&&... args)
+#endif
 {
     return Filler<0, TRet>(func, var, std::forward<TFuncArgs>(args)...);
 }
 
 template<typename TCaseId, std::size_t I, template<std::size_t, 
     std::size_t> class TTArgArg, typename... TArgs>
+#ifdef _WIN32
+template<typename TRet, typename TDerived, typename... TFuncMmbrArgs, 
+        std::size_t ICaseId, typename... TVarArgs, typename TFuncMmbr>
+TRet Argument<TCaseId, arg::type::Index<I, TTArgArg>, TArgs...>::
+    Call(const type::Index<TCaseId, ICaseId>&, TFuncMmbr func_mmbr,
+        TDerived& d, test::Variable<TVarArgs...>& var, TFuncMmbrArgs&&... args)
+#else
 template<typename TRet, typename TDerived, typename... TFuncMmbrArgs, 
         std::size_t ICaseId, typename... TVarArgs>
 TRet Argument<TCaseId, arg::type::Index<I, TTArgArg>, TArgs...>::
@@ -154,6 +218,7 @@ TRet Argument<TCaseId, arg::type::Index<I, TTArgArg>, TArgs...>::
         PointerFunctionMemberType<ICaseId, TRet, TDerived, 
             test::Variable<TVarArgs...>, TFuncMmbrArgs...> func_mmbr, 
         TDerived& d, test::Variable<TVarArgs...>& var, TFuncMmbrArgs&&... args)
+#endif
 {
     return Filler<ICaseId, TRet>(func_mmbr, d, var, 
         std::forward<TFuncMmbrArgs>(args)...);
@@ -161,22 +226,36 @@ TRet Argument<TCaseId, arg::type::Index<I, TTArgArg>, TArgs...>::
 
 template<typename TCaseId, std::size_t I, template<std::size_t, 
     std::size_t> class TTArgArg, typename... TArgs>
-    template<typename TRet, typename... TFuncArgs, std::size_t ICaseId, 
+#ifdef _WIN32
+template<typename TRet, typename... TFuncArgs, std::size_t ICaseId, 
+        typename... TVarArgs, typename TFunc>
+TRet Argument<TCaseId, arg::type::Index<I, TTArgArg>, TArgs...>::
+    Call(const type::Index<TCaseId, ICaseId>&, TFunc func,
+        test::Variable<TVarArgs...>& var, TFuncArgs&&... args)
+#else
+template<typename TRet, typename... TFuncArgs, std::size_t ICaseId, 
         typename... TVarArgs>
 TRet Argument<TCaseId, arg::type::Index<I, TTArgArg>, TArgs...>::
     Call(const type::Index<TCaseId, ICaseId>&, PointerFunctionType<ICaseId, 
         TRet, test::Variable<TVarArgs...>, TFuncArgs...> func, 
         test::Variable<TVarArgs...>& var, TFuncArgs&&... args)
+#endif
 {
     return Filler<ICaseId, TRet>(func, var, std::forward<TFuncArgs>(args)...);
 }
 template<typename TCaseId, std::size_t I, template<std::size_t, 
     std::size_t> class TTArgArg, typename... TArgs>
+#ifdef _WIN32
+template<std::size_t ICaseId, typename... TVarArgs, typename TGet>
+TGet Argument<TCaseId, arg::type::Index<I, TTArgArg>, TArgs...>::
+    Get(test::Variable<TVarArgs...>& var)
+#else
 template<std::size_t ICaseId, typename... TVarArgs>
 typename Argument<TCaseId, arg::type::Index<I, TTArgArg>, TArgs...>::
     template GetType<ICaseId, test::Variable<TVarArgs...>> 
         Argument<TCaseId, arg::type::Index<I, TTArgArg>, TArgs...>::
             Get(test::Variable<TVarArgs...>& var)
+#endif
 {
     return std::move(Argument<TCaseId, TTArgArg<I, ICaseId>>{}.Get(var));
 }
