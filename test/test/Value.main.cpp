e66a99831e566e30bd09d2965aa4bc3b6fbb8392
AnantaYudica <nta040689@gmail.com>
2018-10-09 06:21:42 +0700

    Update test case for /test/Value.h

--- test/test/Value.main.cpp
+++ test/test/Value.main.cpp
@@ -1,22 +1,32 @@
 #include "test/Value.h"
 
-#include <iostream>
+#include <cstdio>
+#include <cassert>
 
 int main()
 {
     basic::test::Value<int> a_int1(4);
-    std::cout << "a_int1.Get() : " << a_int1.Get() << std::endl;
+    printf("a_int1.Get() : %d\n", a_int1.Get());
+    assert(a_int1.Get() == 4);
+
+    a_int1.Get() = 14;
+    printf("a_int1.Get() : %d\n", a_int1.Get());
+    assert(a_int1.Get() == 14);
 
     auto a_int2(a_int1);
-    std::cout << "a_int2.Get() : " << a_int2.Get() << std::endl;
+    printf("a_int2.Get() : %d\n", a_int2.Get());
+    assert(a_int2.Get() == 14);
     
-    basic::test::Value<int> a_int3(14);
-    std::cout << "a_int3.Get() : " << a_int3.Get() << std::endl;
+    basic::test::Value<int> a_int3(44);
+    printf("a_int3.Get() : %d\n", a_int3.Get());
+    assert(a_int3.Get() == 44);
 
     a_int3 = a_int2;
-    std::cout << "a_int3.Get() : " << a_int3.Get() << std::endl;
+    printf("a_int3.Get() : %d\n", a_int3.Get());
+    assert(a_int3.Get() == 14);
 
     const auto a_int4 = a_int3;
-    std::cout << "a_int4.Get() : " << a_int4.Get() << std::endl;
+    printf("a_int4.Get() : %d\n", a_int4.Get());
+    assert(a_int4.Get() == 14);
 
 }
