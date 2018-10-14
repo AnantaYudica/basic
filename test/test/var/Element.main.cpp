51502ed76470e213633a0f6d1a86de027e286407
AnantaYudica <nta040689@gmail.com>
2018-10-14 11:06:13 +0700

    Update test case for test/var/Element.h

--- test/test/var/Element.main.cpp
+++ test/test/var/Element.main.cpp
@@ -1,17 +1,49 @@
 #include "test/var/Element.h"
 #include "test/Variable.h"
 
-#include <iostream>
+#include <cstdio>
+#include <cassert>
 #include <typeinfo>
-
 
 int main()
 {
-    typedef basic::test::Variable<int, char, float> Var1;
-    std::cout << typeid(basic::test::var::Element<0, 
-        Var1>::Type).name() << std::endl;
-    std::cout << typeid(basic::test::var::Element<1, 
-        Var1>::Type).name() << std::endl;
-    std::cout << typeid(basic::test::var::Element<2, 
-        Var1>::Type).name() << std::endl;
+    typedef basic::test::Variable<char> TVar1;
+
+    assert(typeid(char).hash_code() ==
+        typeid(typename basic::test::var::
+            Element<0, TVar1>::Type).hash_code());
+
+    typedef basic::test::Variable<char, short> TVar2;
+    assert(typeid(char).hash_code() ==
+        typeid(typename basic::test::var::
+            Element<0, TVar2>::Type).hash_code());
+    assert(typeid(short).hash_code() ==
+        typeid(typename basic::test::var::
+            Element<1, TVar2>::Type).hash_code());
+
+    typedef basic::test::Variable<char, short, int> TVar3;
+    assert(typeid(char).hash_code() ==
+        typeid(typename basic::test::var::
+            Element<0, TVar3>::Type).hash_code());
+    assert(typeid(short).hash_code() ==
+        typeid(typename basic::test::var::
+            Element<1, TVar3>::Type).hash_code());
+    assert(typeid(int).hash_code() ==
+        typeid(typename basic::test::var::
+            Element<2, TVar3>::Type).hash_code());
+
+    typedef basic::test::Variable<char, short, int, long> TVar4;
+    assert(typeid(char).hash_code() ==
+        typeid(typename basic::test::var::
+            Element<0, TVar4>::Type).hash_code());
+    assert(typeid(short).hash_code() ==
+        typeid(typename basic::test::var::
+            Element<1, TVar4>::Type).hash_code());
+    assert(typeid(int).hash_code() ==
+        typeid(typename basic::test::var::
+            Element<2, TVar4>::Type).hash_code());
+    assert(typeid(long).hash_code() ==
+        typeid(typename basic::test::var::
+            Element<3, TVar4>::Type).hash_code());
+
 }
