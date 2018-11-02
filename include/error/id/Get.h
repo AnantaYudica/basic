339e8aea32341e820202e87c468c20dbee1ceecb
AnantaYudica <nta040689@gmail.com>
2018-11-03 03:47:13 +0700

    Add helper class _helper::_basic::_error::_id::_has::_mmbr::_func::_GetIdentification Add functions basic::error::id::Get with [has member GetIdentification, not has member GetIdentification]

---
+++ include/error/id/Get.h
@@ -0,0 +1,71 @@
+#ifndef BASIC_ERROR_ID_GET_H_
+#define BASIC_ERROR_ID_GET_H_
+
+#include "../macro/HasMemberFunction.h"
+#include "../Identification.h"
+
+#include <type_traits>
+
+namespace _helper
+{
+namespace _basic
+{
+namespace _error
+{
+namespace _id
+{
+namespace _has
+{
+namespace _mmbr
+{
+namespace _func
+{
+
+__DEFINE_HAS_MMBR_FUNC_(_GetIdentification, __GetIdentification, T, TRet, 
+    TArgs, const,,, T_NAME_,,,,, PROTO_FUNC_T_NAME_,, PROTO_FUNC_T_RET_NAME_,, 
+    GetIdentification);
+
+} //!_func
+
+} //!_mmbr
+
+} //!_has
+
+} //!_id
+
+} //!_error
+
+} //!_basic
+
+} //!_helper
+
+namespace basic
+{
+namespace error
+{
+namespace id
+{
+
+template<typename TException>
+constexpr typename std::enable_if<_helper::_basic::_error::_id::_has::
+    _mmbr::_func::_GetIdentification<TException, const Identification&>::Value,
+    Identification>::type Get(const TException& e)
+{
+    return e.GetIdentification();
+}
+
+template<typename TException>
+constexpr typename std::enable_if<!_helper::_basic::_error::_id::_has::
+    _mmbr::_func::_GetIdentification<TException, const Identification&>::Value,
+    Identification>::type Get(const TException& e)
+{
+    return {};
+}
+
+} //!id
+
+} //!error
+
+} //!basic
+
+#endif //!BASIC_ERROR_ID_GET_H_
