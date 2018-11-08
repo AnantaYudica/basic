5e82253117ca1463cba540faa7382f5f33fd2654
AnantaYudica <nta040689@gmail.com>
2018-11-08 17:44:37 +0700

    Add function basic::error::system::category::Message

---
+++ include/error/system/category/Message.h
@@ -0,0 +1,43 @@
+#ifndef BASIC_ERROR_SYSTEM_CATEGORY_MESSAGE_H_
+#define BASIC_ERROR_SYSTEM_CATEGORY_MESSAGE_H_
+
+#include "has/mmbr/func/Message.h"
+#include "Definition.h"
+
+#include <type_traits>
+
+namespace basic
+{
+namespace error
+{
+namespace system
+{
+namespace category
+{
+
+template<typename TCategoryTrait, typename TValue>
+typename std::enable_if<has::mmbr::func::Message<TCategoryTrait,
+    TValue>::Value, const char *>::type  
+Message(const TCategoryTrait& category_trait, const TValue& value)
+{
+    return category_trait.Message(value);
+}
+
+template<typename TCategoryTrait, typename TValue>
+typename std::enable_if<!has::mmbr::func::Message<TCategoryTrait,
+    TValue>::Value, const char *>::type  
+Message(const TCategoryTrait& category_trait, const TValue& value)
+{
+    return "";
+}
+
+} //!category
+
+} //!system
+
+} //!error
+
+} //!basic
+
+
+#endif //!BASIC_ERROR_SYSTEM_CATEGORY_MESSAGE_H_
