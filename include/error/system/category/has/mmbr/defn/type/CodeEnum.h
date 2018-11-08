cbfe77e0b70fcd9e8a1e198d85ee1bc11e4505da
AnantaYudica <nta040689@gmail.com>
2018-11-08 16:30:59 +0700

    Add alias type basic::error::system::category::has::mmbr::defn::type::CodeEnum

---
+++ include/error/system/category/has/mmbr/defn/type/CodeEnum.h
@@ -0,0 +1,84 @@
+#ifndef BASIC_ERROR_SYSTEM_CATEGORY_HAS_MMBR_DEFN_TYPE_CODEENUM_H_
+#define BASIC_ERROR_SYSTEM_CATEGORY_HAS_MMBR_DEFN_TYPE_CODEENUM_H_
+
+#include "../../../../../../../macro/HasMemberDefinition.h"
+
+namespace _helper
+{
+namespace _basic
+{
+namespace _error
+{
+namespace _system
+{
+namespace _category
+{
+namespace _has
+{
+namespace _mmbr
+{
+namespace _defn
+{
+namespace _type
+{
+    
+__DEFINE_HAS_MMBR_DEFN_(_CodeEnum, __CodeEnum, TCategoryTrait,, 
+    T_NAME_,,,,, PROTO_FUNC_T_NAME_,, CodeEnumType);
+
+} //!_type
+
+} //!_defn
+
+} //!_mmbr
+
+} //!_has
+
+} //!_category
+
+} //!_system
+
+} //!_error
+
+} //!_basic
+
+} //!_helper
+
+
+namespace basic
+{
+namespace error
+{
+namespace system
+{
+namespace category
+{
+namespace has
+{
+namespace mmbr
+{
+namespace defn
+{
+namespace type
+{
+
+template<typename TCategoryTrait>
+using CodeEnum = _helper::_basic::_error::_system::_category::_has::_mmbr::
+    _defn::_type::_CodeEnum<TCategoryTrait>;
+
+} //!type
+
+} //!defn
+
+} //!mmbr
+
+} //!has
+
+} //!category
+
+} //!system
+
+} //!error
+
+} //!basic
+
+#endif //!BASIC_ERROR_SYSTEM_CATEGORY_HAS_MMBR_DEFN_TYPE_CODEENUM_H_
