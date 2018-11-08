7c51f0aef66d7072f7bab80c61beb319fd4d512d
AnantaYudica <nta040689@gmail.com>
2018-11-08 17:13:15 +0700

    Add function basic::error::system::category::Equivalent

---
+++ include/error/system/category/Equivalent.h
@@ -0,0 +1,45 @@
+#ifndef BASIC_ERROR_SYSTEM_CATEGORY_EQUIVALENT_H_
+#define BASIC_ERROR_SYSTEM_CATEGORY_EQUIVALENT_H_
+
+#include "has/mmbr/func/Equivalent.h"
+#include "Definition.h"
+
+#include <type_traits>
+
+namespace basic
+{
+namespace error
+{
+namespace system
+{
+namespace category
+{
+
+template<typename TCategoryTrait, typename TCode, typename TCondition>
+typename std::enable_if<has::mmbr::func::Equivalent<TCategoryTrait,
+    TCode, TCondition>::Value, bool>::type  
+Equivalent(const TCategoryTrait& category_trait, const TCode& code,
+    const TCondition& condition)
+{
+    return category_trait.Equivalent(code, condition);
+}
+
+template<typename TCategoryTrait, typename TCode, typename TCondition>
+typename std::enable_if<!has::mmbr::func::Equivalent<TCategoryTrait,
+    TCode, TCondition>::Value, bool>::type   
+Equivalent(const TCategoryTrait& category_trait, const TCode& code,
+    const TCondition& condition)
+{
+    return false;
+}
+
+} //!category
+
+} //!system
+
+} //!error
+
+} //!basic
+
+
+#endif //!BASIC_ERROR_SYSTEM_CATEGORY_EQUIVALENT_H_
