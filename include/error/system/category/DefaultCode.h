b6842ec9b6fd7fda41a4967f59e44e57a683e3ec
AnantaYudica <nta040689@gmail.com>
2018-11-09 15:36:28 +0700

    Update function basic::error::system::category::DefaultCode

--- include/error/system/category/DefaultCode.h
+++ include/error/system/category/DefaultCode.h
@@ -2,7 +2,7 @@
 #define BASIC_ERROR_SYSTEM_CATEGORY_DEFAULTCODE_H_
 
 #include "has/mmbr/func/DefaultCode.h"
-#include "Definition.h"
+#include "defn/type/code/set/Value.h"
 
 #include <type_traits>
 
@@ -17,8 +17,8 @@ namespace category
 
 template<typename TCategoryTrait>
 typename std::enable_if<has::mmbr::func::DefaultCode<TCategoryTrait,
-    typename Definition<TCategoryTrait>::CodeEnumType>::Value, 
-    typename Definition<TCategoryTrait>::CodeEnumType>::type  
+    typename defn::type::code::set::Value<TCategoryTrait>::Type>::Value, 
+    typename defn::type::code::set::Value<TCategoryTrait>::Type>::type  
 DefaultCode(const TCategoryTrait& category_trait)
 {
     return category_trait.DefaultCode();
@@ -26,8 +26,8 @@ DefaultCode(const TCategoryTrait& category_trait)
 
 template<typename TCategoryTrait>
 typename std::enable_if<!has::mmbr::func::DefaultCode<TCategoryTrait,
-    typename Definition<TCategoryTrait>::CodeEnumType>::Value, 
-    typename Definition<TCategoryTrait>::CodeEnumType>::type  
+    typename defn::type::code::set::Value<TCategoryTrait>::Type>::Value, 
+    typename defn::type::code::set::Value<TCategoryTrait>::Type>::type  
 DefaultCode(const TCategoryTrait& category_trait)
 {
     return 0;
