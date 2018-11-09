382264653d4e103acb8334e0e873d71842bd2008
AnantaYudica <nta040689@gmail.com>
2018-11-09 15:07:25 +0700

    Add function basic::error::system::category::Instance

---
+++ include/error/system/category/Instance.h
@@ -0,0 +1,39 @@
+#ifndef BASIC_ERROR_SYSTEM_CATEGORY_INSTANCE_H_
+#define BASIC_ERROR_SYSTEM_CATEGORY_INSTANCE_H_
+
+#include "has/st/mmbr/func/Instance.h"
+
+#include <type_traits>
+
+namespace basic
+{
+namespace error
+{
+namespace system
+{
+namespace category
+{
+
+template<typename TCategoryTrait>
+typename std::enable_if<has::st::mmbr::func::Instance<TCategoryTrait>::Value,
+    TCategoryTrait>::type Instance()
+{
+    return TCategoryTrait::Instance();
+}
+
+template<typename TCategoryTrait>
+typename std::enable_if<has::st::mmbr::func::Instance<TCategoryTrait>::Value,
+    TCategoryTrait>::type Instance()
+{
+    return {};
+}
+
+} //!category
+
+} //!system
+
+} //!error
+
+} //!basic
+
+#endif //!BASIC_ERROR_SYSTEM_CATEGORY_INSTANCE_H_
