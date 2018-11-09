2e422d9919baaec1fe2452a65e2f726897c6507c
AnantaYudica <nta040689@gmail.com>
2018-11-09 10:17:27 +0700

    Add alias type basic::error::system::defn::type::condition::Value

---
+++ include/error/system/defn/type/condition/Value.h
@@ -0,0 +1,33 @@
+#ifndef BASIC_ERROR_SYSTEM_DEFN_TYPE_CONDITION_VALUE_H_
+#define BASIC_ERROR_SYSTEM_DEFN_TYPE_CONDITION_VALUE_H_
+
+#include "../../../../../Error.h"
+
+namespace basic
+{
+namespace error
+{
+namespace system
+{
+namespace defn
+{
+namespace type
+{
+namespace condition
+{
+
+using Value = BASIC_ERROR_SYSTEM_CONDITION_VALUE_TYPE;
+
+} //!code
+
+} //!type
+
+} //!defn
+
+} //!system
+
+} //!error
+
+} //!basic
+
+#endif //!BASIC_ERROR_SYSTEM_DEFN_TYPE_CONDITION_VALUE_H_
