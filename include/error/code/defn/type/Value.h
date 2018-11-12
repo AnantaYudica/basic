681902b943d9289fcc92637ebe2aded4ba7ba185
AnantaYudica <nta040689@gmail.com>
2018-11-12 19:22:48 +0700

    Add alias type basic::error::code::defn::type::Value

---
+++ include/error/code/defn/type/Value.h
@@ -0,0 +1,29 @@
+#ifndef BASIC_ERROR_CODE_DEFN_TYPE_VALUE_H_
+#define BASIC_ERROR_CODE_DEFN_TYPE_VALUE_H_
+
+#include "../../../../Error.h"
+
+namespace basic
+{
+namespace error
+{
+namespace code
+{
+namespace defn
+{
+namespace type
+{
+
+using Value = BASIC_ERROR_CODE_TYPE;
+
+} //!type
+
+} //!defn
+
+} //!code
+
+} //!error
+
+} //!basic
+
+#endif //!BASIC_ERROR_CODE_DEFN_TYPE_VALUE_H_
