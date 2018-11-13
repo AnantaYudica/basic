d4d12e4291db74aa331e7b7593e5096db8507dbb
AnantaYudica <nta040689@gmail.com>
2018-11-13 19:41:45 +0700

    Add alias type basic::error::id::defn::type::Record

---
+++ include/error/id/defn/type/Record.h
@@ -0,0 +1,29 @@
+#ifndef BASIC_ERROR_ID_DEFN_TYPE_RECORD_H_
+#define BASIC_ERROR_ID_DEFN_TYPE_RECORD_H_
+
+#include "../../../../Error.h"
+
+namespace basic
+{
+namespace error
+{
+namespace id
+{
+namespace defn
+{
+namespace type
+{
+
+using Record = BASIC_ERROR_IDENTIFICATION_RECORD_TYPE;
+
+} //!type
+
+} //!defn
+
+} //!id
+
+} //!error
+
+} //!basic
+
+#endif //!BASIC_ERROR_ID_DEFN_TYPE_RECORD_H_
