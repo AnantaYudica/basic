640e6498fcbdb0366f9347f270e87204e7b1d305
AnantaYudica <nta040689@gmail.com>
2018-11-13 17:05:49 +0700

    Update class basic::error::id::rec::SystemError :  - Use template class for definition  - Remove dependencies

--- include/error/id/rec/SystemError.h
+++ include/error/id/rec/SystemError.h
@@ -1,9 +1,6 @@
 #ifndef BASIC_ERROR_ID_REC_SYSTEMERROR_H_
 #define BASIC_ERROR_ID_REC_SYSTEMERROR_H_
 
-#include "../system/defn/type/category/Value.h"
-#include "../system/defn/type/code/Value.h"
-
 #include <utility>
 
 namespace basic
@@ -15,11 +12,12 @@ namespace id
 namespace rec
 {
 
+template<typename TCategoryValue, typename TCodeValue>
 class SystemError
 {
 public:
-    typedef system::defn::type::code::Value CodeValueType;
-    typedef system::defn::type::category::Value CategoryValueType;
+    typedef TCategoryValue CategoryValueType;
+    typedef TCodeValue CodeValueType;
 private:
     CategoryValueType m_categoryValue;
     CodeValueType m_codeValue;
@@ -28,41 +26,51 @@ public:
     SystemError(const CategoryValueType& category_value, 
         const CodeValueType& code_value) noexcept;
 public:
-    SystemError(const SystemError& cpy) noexcept;
-    SystemError(SystemError&& mov) noexcept;
+    SystemError(const SystemError<TCategoryValue, TCodeValue>& cpy) noexcept;
+    SystemError(SystemError<TCategoryValue, TCodeValue>&& mov) noexcept;
 public:
     const CategoryValueType& Category() const noexcept;
     const CodeValueType& Code() const noexcept;
 };
 
-SystemError::SystemError() noexcept :
-    m_categoryValue(0),
-    m_codeValue(0)
+template<typename TCategoryValue, typename TCodeValue>
+SystemError<TCategoryValue, TCodeValue>::SystemError() noexcept :
+    m_categoryValue{0},
+    m_codeValue{0}
 {}
 
-SystemError::SystemError(const CategoryValueType& category_value, 
-    const CodeValueType& code_value) noexcept :
-        m_categoryValue(category_value),
-        m_codeValue(code_value)
+template<typename TCategoryValue, typename TCodeValue>
+SystemError<TCategoryValue, TCodeValue>::
+    SystemError(const CategoryValueType& category_value, 
+        const CodeValueType& code_value) noexcept :
+            m_categoryValue{category_value},
+            m_codeValue{code_value}
 {}
 
-SystemError::SystemError(const SystemError& cpy) noexcept :
-    m_categoryValue(cpy.m_categoryValue),
-    m_codeValue(cpy.m_codeValue)
+template<typename TCategoryValue, typename TCodeValue>
+SystemError<TCategoryValue, TCodeValue>::
+    SystemError(const SystemError<TCategoryValue, TCodeValue>& cpy) noexcept :
+        m_categoryValue{cpy.m_categoryValue},
+        m_codeValue{cpy.m_codeValue}
 {}
 
-SystemError::SystemError(SystemError&& mov) noexcept :
-    m_categoryValue(std::move(mov.m_categoryValue)),
-    m_codeValue(std::move(mov.m_codeValue))
+template<typename TCategoryValue, typename TCodeValue>
+SystemError<TCategoryValue, TCodeValue>::
+    SystemError(SystemError<TCategoryValue, TCodeValue>&& mov) noexcept :
+        m_categoryValue{std::move(mov.m_categoryValue)},
+        m_codeValue{std::move(mov.m_codeValue)}
 {}
 
-const typename SystemError::CategoryValueType& 
-    SystemError::Category() const noexcept
+template<typename TCategoryValue, typename TCodeValue>
+const typename SystemError<TCategoryValue, TCodeValue>::CategoryValueType& 
+SystemError<TCategoryValue, TCodeValue>::Category() const noexcept
 {
     return m_categoryValue;
 }
 
-const typename SystemError::CodeValueType& SystemError::Code() const noexcept
+template<typename TCategoryValue, typename TCodeValue>
+const typename SystemError<TCategoryValue, TCodeValue>::CodeValueType& 
+SystemError<TCategoryValue, TCodeValue>::Code() const noexcept
 {
     return m_codeValue;
 }
