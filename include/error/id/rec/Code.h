7dab5c81450af42f5d15f4d8489ec728e6ce1983
AnantaYudica <nta040689@gmail.com>
2018-11-13 17:04:18 +0700

    Update union basic::error::id::rec::Code :  - Use template union for definition  - Remove dependencies

--- include/error/id/rec/Code.h
+++ include/error/id/rec/Code.h
@@ -1,9 +1,6 @@
 #ifndef BASIC_ERROR_ID_REC_CODE_H_
 #define BASIC_ERROR_ID_REC_CODE_H_
 
-#include "../code/defn/type/Value.h"
-#include "../system/defn/type/code/Value.h"
-
 #include <utility>
 
 namespace basic
@@ -15,36 +12,46 @@ namespace id
 namespace rec
 {
 
+template<typename TErrorCodeValue, typename TSystemErrorCodeValue>
 union Code
 {
-    error::code::defn::type::Value Error;
-    error::system::defn::type::code::Value SystemError;
+    TErrorCodeValue Error;
+    TSystemErrorCodeValue SystemError;
     constexpr Code() noexcept;
-    constexpr Code(const error::code::defn::type::Value& error,
-        const error::system::defn::type::code::Value& system_error) noexcept;
-    constexpr Code(const Code& cpy) noexcept;
-    constexpr Code(Code&& mov) noexcept;
+    constexpr Code(const TErrorCodeValue& error, 
+        const TSystemErrorCodeValue& system_error) noexcept;
+    constexpr Code(const Code<TErrorCodeValue, 
+        TSystemErrorCodeValue>& cpy) noexcept;
+    constexpr Code(Code<TErrorCodeValue, 
+        TSystemErrorCodeValue>&& mov) noexcept;
 };
 
-constexpr Code::Code() noexcept :
-    Error(0),
-    SystemError(0)
+template<typename TErrorCodeValue, typename TSystemErrorCodeValue>
+constexpr Code<TErrorCodeValue, TSystemErrorCodeValue>::Code() noexcept :
+    Error{0},
+    SystemError{0}
 {}
 
-constexpr Code::Code(const error::code::defn::type::Value& error,
-    const error::system::defn::type::code::Value& system_error) noexcept :
-        Error(error),
-        SystemError(SystemError | system_error)
+template<typename TErrorCodeValue, typename TSystemErrorCodeValue>
+constexpr Code<TErrorCodeValue, TSystemErrorCodeValue>::
+    Code(const TErrorCodeValue& error, 
+        const TSystemErrorCodeValue& system_error) noexcept :
+            Error{error},
+            SystemError{SystemError | system_error}
 {}
 
-constexpr Code::Code(const Code& cpy) noexcept :
-    Error(cpy.Error),
-    SystemError(SystemError | cpy.SystemError)
+template<typename TErrorCodeValue, typename TSystemErrorCodeValue>
+constexpr Code<TErrorCodeValue, TSystemErrorCodeValue>::
+    Code(const Code<TErrorCodeValue, TSystemErrorCodeValue>& cpy) noexcept :
+        Error{cpy.Error},
+        SystemError{SystemError | cpy.SystemError}
 {}
 
-constexpr Code::Code(Code&& mov) noexcept :
-    Error(mov.Error),
-    SystemError(SystemError | mov.SystemError)
+template<typename TErrorCodeValue, typename TSystemErrorCodeValue>
+constexpr Code<TErrorCodeValue, TSystemErrorCodeValue>::
+    Code(Code<TErrorCodeValue, TSystemErrorCodeValue>&& mov) noexcept :
+        Error{mov.Error},
+        SystemError{SystemError | mov.SystemError}
 {}
 
 } //!rec
