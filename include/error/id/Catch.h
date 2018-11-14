71a360a05eb761e3a61e07bfc79e4351ac1afabd
AnantaYudica <nta040689@gmail.com>
2018-11-14 21:46:05 +0700

    Add dependencies Update class basic::error::id::Catch

--- include/error/id/Catch.h
+++ include/error/id/Catch.h
@@ -1,7 +1,11 @@
-#ifndef BASIC_ERROR_IDENTIFICATION_CATCH_H_
-#define BASIC_ERROR_IDENTIFICATION_CATCH_H_
+#ifndef BASIC_ERROR_ID_CATCH_H_
+#define BASIC_ERROR_ID_CATCH_H_
 
+#include "../code/defn/type/Value.h"
+#include "../system/defn/type/category/Value.h"
+#include "../system/defn/type/code/Value.h"
 #include "../Identification.h"
+#include "../id/Flag.h"
 
 #include <utility>
 
@@ -15,31 +19,72 @@ namespace id
 class Catch : public Identification
 {
 public:
-    constexpr Catch();
-    constexpr Catch(const Identification& id);
+    typedef code::defn::type::Value CodeValueType;
+    typedef system::defn::type::category::Value SystemCategoryValueType;
+    typedef system::defn::type::code::Value SystemCodeValueType;
 public:
-    Catch(const Catch& cpy);
-    Catch(Catch&& mov);
+    constexpr Catch() noexcept;
+    constexpr Catch(const CodeValueType& code_val) noexcept;
+    constexpr Catch(const flag::Standard&, 
+        const CodeValueType& code_val) noexcept;
+    constexpr Catch(const flag::System&, 
+        const SystemCategoryValueType& system_category_val,
+        const SystemCodeValueType& system_code_val) noexcept;
+    constexpr Catch(const flag::System&, const flag::Standard&, 
+        const SystemCategoryValueType& system_category_val,
+        const SystemCodeValueType& system_code_val) noexcept;
+    constexpr Catch(const Identification& id) noexcept;
+    constexpr Catch(const flag::Standard&, const Identification& id) noexcept;
 public:
-    ~Catch();
+    Catch(const Catch& cpy) noexcept;
+    Catch(Catch&& mov) noexcept;
 public:
     Catch& operator=(const Catch&) = delete;
     Catch& operator=(Catch&&) = delete;
 };
 
-constexpr Catch::Catch() :
+constexpr Catch::Catch() noexcept :
     Identification()
 {}
 
-constexpr Catch::Catch(const Identification& id) :
-    Identification(id.Flag.Default, id.Flag.Standard, 1, id.Error)
+constexpr Catch::Catch(const CodeValueType& code_val) noexcept :
+    Identification(flag::Catch(), code_val)
 {}
 
-Catch::Catch(const Catch& cpy) :
+constexpr Catch::Catch(const flag::Standard& standard, 
+    const CodeValueType& code_val) noexcept :
+        Identification(flag::Catch(), standard, code_val)
+{}
+
+constexpr Catch::Catch(const flag::System& system, 
+    const SystemCategoryValueType& system_category_val,
+    const SystemCodeValueType& system_code_val) noexcept :
+        Identification(flag::Catch(), system, system_category_val, 
+            system_code_val)
+{}
+
+constexpr Catch::Catch(const flag::System& system, 
+    const flag::Standard& standard, 
+    const SystemCategoryValueType& system_category_val,
+    const SystemCodeValueType& system_code_val) noexcept :
+        Identification(flag::Catch(), system, standard,  
+            system_category_val, system_code_val)
+{}
+
+constexpr Catch::Catch(const Identification& id) noexcept :
+    Identification(flag::Catch(), id)
+{}
+
+constexpr Catch::Catch(const flag::Standard& standard, 
+    const Identification& id) noexcept :
+        Identification(flag::Catch(), standard, id)
+{}
+
+Catch::Catch(const Catch& cpy) noexcept :
     Identification(cpy)
 {}
 
-Catch::Catch(Catch&& mov) :
+Catch::Catch(Catch&& mov) noexcept :
     Identification(std::move(mov))
 {}
 
@@ -49,4 +94,4 @@ Catch::Catch(Catch&& mov) :
 
 } //!basic
 
-#endif //!BASIC_ERROR_IDENTIFICATION_CATCH_H_
+#endif //!BASIC_ERROR_ID_CATCH_H_
