da0d9f26c30c51e2bbdca039891843a2e622f2c8
AnantaYudica <nta040689@gmail.com>
2018-11-16 17:26:48 +0700

    Update class basic::error::Code

--- include/error/Code.h
+++ include/error/Code.h
@@ -1,8 +1,8 @@
 #ifndef BASIC_ERROR_CODE_H_
 #define BASIC_ERROR_CODE_H_
 
-#include <cstdint>
-#include <type_traits>
+#include "code/defn/type/Value.h"
+
 #include <utility>
 
 namespace basic
@@ -10,157 +10,109 @@ namespace basic
 namespace error
 {
 
-template<typename TInteger = std::uint16_t,
-    TInteger DefaultInteger = 0>
 class Code
 {
-    static_assert(std::is_integral<TInteger>::value, 
-        "check integer type");
 public:
-    typedef TInteger IntegerType;
-public:
-    static constexpr IntegerType DefaultValue = DefaultInteger;
+    typedef code::defn::type::Value CodeValueType;
 private:
-    IntegerType m_value;
+    CodeValueType m_value;
 public:
-    constexpr Code();
-    constexpr Code(const IntegerType& value);
+    constexpr Code() noexcept;
+    constexpr Code(const CodeValueType& value) noexcept;
 public:
-    Code(const Code<TInteger, DefaultInteger>& cpy);
-    Code(Code<TInteger, DefaultInteger>&& mov);
+    Code(const Code& cpy) noexcept;
+    Code(Code&& mov) noexcept;
 public:
-    ~Code();
+    Code& operator=(const Code& cpy) noexcept;
+    Code& operator=(Code&& mov) noexcept;
 public:
-    Code<TInteger, DefaultInteger>& 
-        operator=(const Code<TInteger, DefaultInteger>& cpy);
-    Code<TInteger, DefaultInteger>& 
-        operator=(Code<TInteger, DefaultInteger>&& mov);
+    Code& operator=(const CodeValueType& value) noexcept;
 public:
-    Code<TInteger, DefaultInteger>& operator=(const TInteger& value);
-public:
-    operator TInteger&();
-    operator const TInteger&() const;
-public:
-    bool operator==(const Code<TInteger, DefaultInteger>& other) const;
-    bool operator!=(const Code<TInteger, DefaultInteger>& other) const;
-    template<typename TIntegerOther>
-    bool operator==(const TIntegerOther& other) const;
-    template<typename TIntegerOther>
-    bool operator!=(const TIntegerOther& other) const;
+    CodeValueType Value() const noexcept;
 };
 
-template<typename TInteger, TInteger DefaultInteger>
-constexpr Code<TInteger, DefaultInteger>::Code() :
-    m_value(DefaultInteger)
+constexpr Code::Code() noexcept :
+    m_value(0)
 {}
 
-template<typename TInteger, TInteger DefaultInteger>
-constexpr Code<TInteger, DefaultInteger>::Code(const IntegerType& value) :
+constexpr Code::Code(const CodeValueType& value) noexcept :
     m_value(value)
 {}
 
-template<typename TInteger, TInteger DefaultInteger>
-Code<TInteger, DefaultInteger>::Code(const Code<TInteger, 
-    DefaultInteger>& cpy) :
-        m_value(cpy.m_value)
+Code::Code(const Code& cpy) noexcept :
+    m_value(cpy.m_value)
 {}
 
-template<typename TInteger, TInteger DefaultInteger>
-Code<TInteger, DefaultInteger>::Code(Code<TInteger, DefaultInteger>&& mov) :
-    m_value(std::move(mov.m_value))
+Code::Code(Code&& mov) noexcept :
+    m_value(std::move(mov.m_value)) 
 {
-    mov.m_value = DefaultInteger;
+    mov.m_value = 0;
 }
 
-template<typename TInteger, TInteger DefaultInteger>
-Code<TInteger, DefaultInteger>::~Code()
-{}
-
-template<typename TInteger, TInteger DefaultInteger>
-Code<TInteger, DefaultInteger>& Code<TInteger, DefaultInteger>::
-    operator=(const Code<TInteger, DefaultInteger>& cpy)
+Code& Code::operator=(const Code& cpy) noexcept
 {
     this->m_value = cpy.m_value;
     return *this;
 }
 
-template<typename TInteger, TInteger DefaultInteger>
-Code<TInteger, DefaultInteger>& Code<TInteger, DefaultInteger>::
-    operator=(Code<TInteger, DefaultInteger>&& mov)
+Code& Code::operator=(Code&& mov) noexcept
 {
     this->m_value = std::move(mov.m_value);
-    mov.m_value = DefaultInteger;
+    mov.m_value = 0;
     return *this;
 }
 
-template<typename TInteger, TInteger DefaultInteger>
-Code<TInteger, DefaultInteger>& Code<TInteger, DefaultInteger>::
-    operator=(const TInteger& value)
+Code& Code::operator=(const CodeValueType& value) noexcept
 {
     this->m_value = value;
     return *this;
 }
 
-template<typename TInteger, TInteger DefaultInteger>
-Code<TInteger, DefaultInteger>::operator TInteger&()
+typename Code::CodeValueType Code::Value() const noexcept
 {
     return this->m_value;
-}
-
-template<typename TInteger, TInteger DefaultInteger>
-Code<TInteger, DefaultInteger>::operator const TInteger&() const
-{
-    return this->m_value;
-}
-
-template<typename TInteger, TInteger DefaultInteger>
-bool Code<TInteger, DefaultInteger>::
-    operator==(const Code<TInteger, DefaultInteger>& other) const
-{
-    return this->m_value == other.m_value;
-}
-
-template<typename TInteger, TInteger DefaultInteger>
-bool Code<TInteger, DefaultInteger>::
-    operator!=(const Code<TInteger, DefaultInteger>& other) const
-{
-    return !(*this == other);
-}
-
-template<typename TInteger, TInteger DefaultInteger>
-template<typename TIntegerOther>
-bool Code<TInteger, DefaultInteger>::
-    operator==(const TIntegerOther& other) const
-{
-    return this->m_value == other;
-}
-
-template<typename TInteger, TInteger DefaultInteger>
-template<typename TIntegerOther>
-bool Code<TInteger, DefaultInteger>::
-    operator!=(const TIntegerOther& other) const
-{
-    return !(*this == other);
 }
 
 } //!error
 
 } //!basic
 
-template<typename TInteger, typename TIntegerOther, 
-    TIntegerOther DefaultIntegerOther>
-bool operator==(const TInteger& code, 
-    const basic::error::Code<TIntegerOther, DefaultIntegerOther>& code_other)
+bool operator==(const basic::error::Code& code, 
+    const basic::error::Code& code_other)
 {
-    return code_other == code;
+    return code.Value() == code_other.Value();
 }
 
-template<typename TInteger, typename TIntegerOther,
-    TIntegerOther DefaultIntegerOther>
-bool operator!=(const TInteger& code, 
-    const basic::error::Code<TIntegerOther, DefaultIntegerOther>& code_other)
+template<typename TValueCode>
+bool operator==(const basic::error::Code& code, const TValueCode& code_val)
 {
-    return code_other == code;
+    return code.Value() == code_val;
+}
+
+template<typename TValueCode>
+bool operator==(const TValueCode& code_val, 
+    const basic::error::Code& code_other)
+{
+    return code_val == code_other.Value();
+}
+
+bool operator!=(const basic::error::Code& code, 
+    const basic::error::Code& code_other)
+{
+    return !(code == code_other);
+}
+
+template<typename TValueCode>
+bool operator!=(const basic::error::Code& code, const TValueCode& code_val)
+{
+    return !(code == code_val);
+}
+
+template<typename TValueCode>
+bool operator!=(const TValueCode& code_val, 
+    const basic::error::Code& code_other)
+{
+    return !(code_val == code_other);
 }
 
 #endif //!BASIC_ERROR_CODE_H_
