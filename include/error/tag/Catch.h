ea862c5153e41c163a1cd643e052d48ce0b09120
AnantaYudica <nta040689@gmail.com>
2018-11-26 00:57:00 +0700

    Update class specialized basic::Error<basic::error::tag::Catch<?>>  - Add and Update dependencies  - Update and Remove alias type member  - Update body function  - Update deleted function operator=

--- include/error/tag/Catch.h
+++ include/error/tag/Catch.h
@@ -9,6 +9,7 @@
 
 #include "Trigger.h"
 #include "../id/Catch.h"
+#include "../defn/type/Output.h"
 
 #include <ostream>
 #include <exception>
@@ -34,9 +35,7 @@ class Error<error::tag::Catch<TTrigger>> :
 public:
     typedef Error<error::tag::Trigger> BaseType;
 public:
-    typedef BASIC_ERROR_CHAR_TYPE CharType;
-    typedef BASIC_ERROR_CHARTRAIT_TYPE CharTraitType;
-    typedef BASIC_ERROR_OUTPUT_TYPE OutputType;
+    typedef error::defn::type::Output OutputType;
 private:
     TTrigger m_trigger;
 public:
@@ -47,10 +46,10 @@ public:
     Error(const Error<error::tag::Catch<TTrigger>>& cpy) noexcept;
     Error(Error<error::tag::Catch<TTrigger>>&& mov) noexcept;
 public:
-    Error<error::tag::Catch>& 
-        operator=(const Error<error::tag::Catch>&) = delete;
-    Error<error::tag::Catch>& 
-        operator=(Error<error::tag::Catch>&&) = delete;
+    Error<error::tag::Catch<TTrigger>>& 
+        operator=(const Error<error::tag::Catch<TTrigger>>&) = delete;
+    Error<error::tag::Catch<TTrigger>>& 
+        operator=(Error<error::tag::Catch<TTrigger>>&&) = delete;
 public:
     OutputType& Message(OutputType& out) const noexcept;
 public:
@@ -67,8 +66,8 @@ template<typename TTrigger>
 Error<error::tag::Catch<TTrigger>>::Error(const TTrigger& trigger, 
     const error::id::Catch& id, const char* file, const std::size_t& line) 
         noexcept :
-            m_trigger(err),
-            BaseType(id, file, line),
+            m_trigger(trigger),
+            BaseType(id, file, line)
 {}
 
 template<typename TTrigger>
@@ -76,7 +75,7 @@ typename Error<error::tag::Catch<TTrigger>>::OutputType&
     Error<error::tag::Catch<TTrigger>>::Message(OutputType& out) const noexcept
 {
     BASIC_ERROR_OUTPUT_OPERATOR(out, "in catch ");
-    BASIC_ERROR_OUTPUT_OPERATOR(out, this->GetInformation());
+    BASIC_ERROR_OUTPUT_OPERATOR(out, this->Information());
     return out;
 }
 
