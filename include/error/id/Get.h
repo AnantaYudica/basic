2a70aa405dbe76209578d83e7b0f08585b316b00
AnantaYudica <nta040689@gmail.com>
2018-11-27 01:04:07 +0700

    Update dependency Add noexcept in function Get

--- include/error/id/Get.h
+++ include/error/id/Get.h
@@ -1,7 +1,7 @@
 #ifndef BASIC_ERROR_ID_GET_H_
 #define BASIC_ERROR_ID_GET_H_
 
-#include "../macro/HasMemberFunction.h"
+#include "../../macro/HasMemberFunction.h"
 #include "../Identification.h"
 
 #include <type_traits>
@@ -49,7 +49,7 @@ namespace id
 template<typename TException>
 constexpr typename std::enable_if<_helper::_basic::_error::_id::_has::
     _mmbr::_func::_GetIdentification<TException, const Identification&>::Value,
-    Identification>::type Get(const TException& e)
+    Identification>::type Get(const TException& e) noexcept
 {
     return e.GetIdentification();
 }
@@ -57,7 +57,7 @@ constexpr typename std::enable_if<_helper::_basic::_error::_id::_has::
 template<typename TException>
 constexpr typename std::enable_if<!_helper::_basic::_error::_id::_has::
     _mmbr::_func::_GetIdentification<TException, const Identification&>::Value,
-    Identification>::type Get(const TException& e)
+    Identification>::type Get(const TException& e) noexcept
 {
     return {};
 }
