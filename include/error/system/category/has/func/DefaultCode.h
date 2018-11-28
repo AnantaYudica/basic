04c6d98201593e70753ad97393ef2f018f818530
AnantaYudica <nta040689@gmail.com>
2018-11-29 01:32:42 +0700

    Add class basic::error::system::category::has::func::DefaultCode

---
+++ include/error/system/category/has/func/DefaultCode.h
@@ -0,0 +1,41 @@
+#ifndef BASIC_ERROR_SYSTEM_CATEGORY_HAS_FUNC_DEFAULTCODE_H_
+#define BASIC_ERROR_SYSTEM_CATEGORY_HAS_FUNC_DEFAULTCODE_H_
+
+#include "../../defn/type/code/set/Value.h"
+#include "../mmbr/func/DefaultCode.h"
+#include "../mmbr/defn/type/CodeEnum.h"
+
+namespace basic
+{
+namespace error
+{
+namespace system
+{
+namespace category
+{
+namespace has
+{
+namespace func
+{
+
+template<typename TCategoryTrait>
+struct DefaultCode
+{
+    static constexpr bool Value = has::mmbr::func::DefaultCode<TCategoryTrait,
+        typename defn::type::code::set::Value<TCategoryTrait>::Type>::Value || 
+        !has::mmbr::defn::type::CodeEnum<TCategoryTrait>::Value;
+};
+
+} //!func
+
+} //!has
+
+} //!category
+
+} //!system
+
+} //!error
+
+} //!basic
+
+#endif //!BASIC_ERROR_SYSTEM_CATEGORY_HAS_FUNC_DEFAULTCODE_H_
