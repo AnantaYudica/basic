55b844693826f8844eef22ce2c12fa54dd44c554
AnantaYudica <nta040689@gmail.com>
2018-12-02 18:11:55 +0700

    Add function basic::error::msg::str::Allocate

---
+++ include/error/msg/str/Allocate.h
@@ -0,0 +1,40 @@
+#ifndef BASIC_ERROR_MSG_STR_ALLOCATE_H_
+#define BASIC_ERROR_MSG_STR_ALLOCATE_H_
+
+#include "../../defn/type/Char.h"
+
+#include <cstddef>
+
+namespace basic
+{
+namespace error
+{
+namespace msg
+{
+namespace str
+{
+
+void Allocate(defn::type::Char *(&storage_ref), 
+    const std::size_t& size) noexcept
+{
+    storage_ref = new defn::type::Char[size];
+    if (storage_ref != nullptr)
+        storage_ref[0] = '\0';
+}
+
+template<std::size_t N>
+void Allocate(defn::type::Char (&storage_ref)[N], 
+    const std::size_t& size) noexcept
+{
+    storage_ref[0] = '\0';
+}
+
+} //!str
+
+} //!msg
+
+} //!error
+
+} //!basic
+
+#endif //!BASIC_ERROR_MSG_STR_ALLOCATE_H_
