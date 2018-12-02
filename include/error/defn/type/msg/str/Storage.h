63415e18e0c61b6c75de92b7f2be925b8208fc80
AnantaYudica <nta040689@gmail.com>
2018-12-03 06:04:22 +0700

    Add alias type basic::error::defn::type::msg::str::Storage

---
+++ include/error/defn/type/msg/str/Storage.h
@@ -0,0 +1,34 @@
+#ifndef BASIC_ERROR_DEFN_TYPE_MSG_STR_STORAGE_H_
+#define BASIC_ERROR_DEFN_TYPE_MSG_STR_STORAGE_H_
+
+#include "../../../../Definition.h"
+
+namespace basic
+{
+namespace error
+{
+namespace defn
+{
+namespace type
+{
+namespace msg
+{
+namespace str
+{
+
+using Storage = BASIC_ERROR_MSG_STR_STORAGE_TYPE;
+
+} //!str
+
+} //!msg
+
+} //!type
+
+} //!defn
+
+} //!error
+
+} //!basic
+
+
+#endif //!BASIC_ERROR_DEFN_TYPE_MSG_STR_STORAGE_H_
