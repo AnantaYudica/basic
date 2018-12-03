751f323ce6782a1ed597f77248072c09e5c1a277
AnantaYudica <nta040689@gmail.com>
2018-12-03 14:58:38 +0700

    Add prepro with USING_BASIC_ERROR_FILE_AND_LINE

--- include/error/Information.h
+++ include/error/Information.h
@@ -22,12 +22,18 @@ public:
     typedef defn::type::Output OutputValueType;
 private:
     const error::Identification m_id;
-    const char* m_file;
+#ifdef USING_BASIC_ERROR_FILE_AND_LINE
+    const char * m_file;
     const std::size_t m_line;
+#endif //!USING_BASIC_ERROR_FILE_AND_LINE
 public:
     constexpr Information() noexcept;
+#ifdef USING_BASIC_ERROR_FILE_AND_LINE
     constexpr Information(const error::Identification& id, const char* file,
         const std::size_t& line) noexcept;
+#else
+    constexpr Information(const error::Identification& id) noexcept;
+#endif //!USING_BASIC_ERROR_FILE_AND_LINE
 public:
     Information(const Information& cpy) noexcept;
     Information(Information&& mov) noexcept;
@@ -36,35 +42,47 @@ public:
     Information& operator=(Information&& mov) = delete;
 public:
     const error::Identification& Identification() const noexcept;
-    const char* File() const noexcept;
-    const std::size_t& Line() const noexcept;
+    const char * File() const noexcept;
+    std::size_t Line() const noexcept;
 protected:
     virtual OutputValueType& Output(OutputValueType& out) const noexcept;
 };
 
 constexpr Information::Information() noexcept :
-    m_id(),
+#ifdef USING_BASIC_ERROR_FILE_AND_LINE
     m_file(nullptr),
-    m_line((std::size_t)-1)
+    m_line(static_cast<std::size_t>(-1)),
+#endif //!USING_BASIC_ERROR_FILE_AND_LINE
+    m_id()
 {}
 
+#ifdef USING_BASIC_ERROR_FILE_AND_LINE
 constexpr Information::Information(const error::Identification& id, 
     const char* file, const std::size_t& line) noexcept :
-        m_id(id),
         m_file(file),
-        m_line(line)
+        m_line(line),
+        m_id(id)
 {}
+#else
+constexpr Information::Information(const error::Identification& id) noexcept :
+    m_id(id)
+{}
+#endif //!USING_BASIC_ERROR_FILE_AND_LINE
 
 Information::Information(const Information& cpy) noexcept :
-    m_id(cpy.m_id),
+#ifdef USING_BASIC_ERROR_FILE_AND_LINE
     m_file(cpy.m_file),
-    m_line(cpy.m_line)
+    m_line(cpy.m_line),
+#endif //!USING_BASIC_ERROR_FILE_AND_LINE
+    m_id(cpy.m_id)
 {}
 
 Information::Information(Information&& mov) noexcept :
-    m_id(mov.m_id),
+#ifdef USING_BASIC_ERROR_FILE_AND_LINE
     m_file(mov.m_file),
-    m_line(mov.m_line)
+    m_line(mov.m_line),
+#endif //!USING_BASIC_ERROR_FILE_AND_LINE
+    m_id(mov.m_id)
 {}
 
 const error::Identification& Information::Identification() const noexcept
@@ -74,12 +92,20 @@ const error::Identification& Information::Identification() const noexcept
 
 const char* Information::File() const noexcept
 {
+#ifdef USING_BASIC_ERROR_FILE_AND_LINE
     return m_file;
+#else  
+    return "";
+#endif //!USING_BASIC_ERROR_FILE_AND_LINE
 }
 
-const std::size_t& Information::Line() const noexcept
+std::size_t Information::Line() const noexcept
 {
+#ifdef USING_BASIC_ERROR_FILE_AND_LINE
     return m_line;
+#else
+    return static_cast<std::size_t>(-1);
+#endif //!USING_BASIC_ERROR_FILE_AND_LINE
 }
 
 typename Information::OutputValueType& 
@@ -89,8 +115,10 @@ Information::Output(OutputValueType& out) const noexcept
     {
         if (!this->m_id.IsCatch())
             output::Operator(out, "id ", this->m_id);
+#ifdef USING_BASIC_ERROR_FILE_AND_LINE
         output::Operator(out, "file ", this->m_file);
         output::Operator(out, "line ", this->m_file);
+#endif //!USING_BASIC_ERROR_FILE_AND_LINE
     }
     return out;
 }
