d2d97e222a00b14fc7e998f471dbbc2302eaa399
AnantaYudica <nta040689@gmail.com>
2018-12-05 15:30:22 +0700

    Update class basic::error::tag::Catch

--- include/error/tag/Catch.h
+++ include/error/tag/Catch.h
@@ -9,12 +9,12 @@
 
 #include "Trigger.h"
 #include "../id/Catch.h"
+#include "../defn/type/Char.h"
 #include "../defn/type/Output.h"
 #include "../output/Interface.h"
 #include "../output/Operator.h"
 
-#include <ostream>
-#include <exception>
+#include <utility>
 
 namespace basic
 {
@@ -37,13 +37,15 @@ class Error<error::tag::Catch<TTrigger>> :
 public:
     typedef Error<error::tag::Trigger> TriggerType;
 public:
+    typedef error::defn::type::Char CharType;
     typedef error::defn::type::Output OutputValueType;
 private:
     TTrigger m_trigger;
 public:
     Error() noexcept;
+    template<typename... TArgs>
     Error(const TTrigger& trigger, const error::id::Catch& id, 
-        const char* file, const std::size_t& line) noexcept;
+        TArgs&&... args) noexcept;
 public:
     Error(const Error<error::tag::Catch<TTrigger>>& cpy) noexcept;
     Error(Error<error::tag::Catch<TTrigger>>&& mov) noexcept;
@@ -53,11 +55,11 @@ public:
     Error<error::tag::Catch<TTrigger>>& 
         operator=(Error<error::tag::Catch<TTrigger>>&&) = delete;
 public:
-    const char * Message() const noexcept;
+    const CharType * Message() const noexcept;
 public:
-    const TTrigger& Trigger() const noexcept;
+    const TTrigger & Trigger() const noexcept;
 protected:
-    OutputValueType& Output(OutputValueType& out) const noexcept;
+    OutputValueType & Output(OutputValueType& out) const noexcept;
 };
 
 template<typename TTrigger>
@@ -67,15 +69,31 @@ Error<error::tag::Catch<TTrigger>>::Error() noexcept :
 {}
 
 template<typename TTrigger>
+template<typename... TArgs>
 Error<error::tag::Catch<TTrigger>>::Error(const TTrigger& trigger, 
-    const error::id::Catch& id, const char* file, const std::size_t& line) 
-        noexcept :
-            m_trigger(trigger),
-            TriggerType(id, file, line)
+    const error::id::Catch& id, TArgs&&... args) noexcept :
+        m_trigger(trigger),
+        TriggerType(id, std::forward<TArgs>(args)...)
 {}
 
 template<typename TTrigger>
-const char * Error<error::tag::Catch<TTrigger>>::Message() const noexcept
+Error<error::tag::Catch<TTrigger>>::
+    Error(const Error<error::tag::Catch<TTrigger>>& cpy) noexcept :
+        m_trigger(cpy.m_trigger),
+        TriggerType(cpy)
+
+{}
+
+template<typename TTrigger>
+Error<error::tag::Catch<TTrigger>>::
+    Error(Error<error::tag::Catch<TTrigger>>&& mov) noexcept :
+        m_trigger(std::move(mov.m_trigger)),
+        TriggerType(std::move(mov))
+{}
+
+template<typename TTrigger>
+const typename Error<error::tag::Catch<TTrigger>>::CharType * 
+Error<error::tag::Catch<TTrigger>>::Message() const noexcept
 {
     return "";
 }
@@ -87,7 +105,7 @@ const TTrigger& Error<error::tag::Catch<TTrigger>>::Trigger() const noexcept
 }
 
 template<typename TTrigger>
-typename Error<error::tag::Catch<TTrigger>>::OutputValueType& 
+typename Error<error::tag::Catch<TTrigger>>::OutputValueType & 
 Error<error::tag::Catch<TTrigger>>::Output(OutputValueType& out) const noexcept
 {
     error::output::Operator(out, "in catch ");
