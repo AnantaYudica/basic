0d3cbea2622c04be7f00ee1b459ab3158f05ecb6
AnantaYudica <nta040689@gmail.com>
2018-12-06 20:54:03 +0700

    Update class basic::error::exception::logic::Domain Revert class basic::error::exception::logic::Domain if defined USING_STANDARD_EXCEPTION to alias type

--- include/error/exception/logic/Domain.h
+++ include/error/exception/logic/Domain.h
@@ -5,8 +5,10 @@
 #include "../../Identification.h"
 #include "../../id/Get.h"
 #include "../../id/Standard.h"
+#include "../../defn/type/Char.h"
 #include "../../defn/type/Output.h"
 #include "../../output/Operator.h"
+#include "../../msg/String.h"
 #include "../../../constant/error/Identification.h"
 
 #include <stdexcept>
@@ -28,62 +30,75 @@ class Domain : public exception::Logic
 public:
     typedef typename error::Exception::TriggerType TriggerType;
 public:
+    typedef defn::type::Char CharType;
     typedef defn::type::Output OutputValueType;
 protected:
     Domain() noexcept;
-    Domain(const char * message) noexcept;
-public:
-    Domain(const char* file, const std::size_t& line) noexcept;
-    Domain(const char * message, const char* file, 
-        const std::size_t& line) noexcept;
-public:
-    Domain(const Domain& cpy) noexcept;
-    Domain(Domain&& mov) noexcept;
-public:
-    Domain& operator=(const Domain&) = delete;
-    Domain& operator=(Domain&&) = delete;
-public:
-    virtual const char* Message() const noexcept;
+
+#ifdef USING_BASIC_ERROR_FILE_AND_LINE
+
 protected:
-    virtual OutputValueType& Output(OutputValueType& out) const noexcept;
+    Domain(const CharType * message) noexcept;
+public:
+    Domain(const CharType * message, const char * file, 
+        const std::size_t & line) noexcept;
+
+#else
+
+public:
+    Domain(const CharType * message) noexcept;
+
+#endif //!USING_BASIC_ERROR_FILE_AND_LINE
+
+public:
+    Domain(const Domain & cpy) noexcept;
+    Domain(Domain && mov) noexcept;
+public:
+    Domain& operator=(const Domain &) = delete;
+    Domain& operator=(Domain &&) = delete;
+public:
+    virtual const CharType * Message() const noexcept;
+protected:
+    virtual OutputValueType & Output(OutputValueType & out) const noexcept;
 };
 
 Domain::Domain() noexcept :
+    TriggerType(constant::error::logic_domain_id),
     exception::Logic("Domain Logic Exception")
 {}
 
-Domain::Domain(const char * message) noexcept :
+Domain::Domain(const CharType * message) noexcept :
+    TriggerType(constant::error::logic_domain_id),
     exception::Logic(message)
 {}
 
-Domain::Domain(const char* file, const std::size_t& line) noexcept :
-    TriggerType(constant::error::logic_domain_id, file, line),
-    exception::Logic("Domain Logic Exception")
-{}
+#ifdef USING_BASIC_ERROR_FILE_AND_LINE
 
-Domain::Domain(const char * message, const char* file, 
-    const std::size_t& line) noexcept :
+Domain::Domain(const CharType * message, const char * file, 
+    const std::size_t & line) noexcept :
         TriggerType(constant::error::logic_domain_id, file, line),
         exception::Logic(message)
 {}
 
-Domain::Domain(const Domain& cpy) noexcept :
+#endif //!USING_BASIC_ERROR_FILE_AND_LINE
+
+Domain::Domain(const Domain & cpy) noexcept :
     TriggerType(cpy),
     exception::Logic(cpy)
 {}
 
-Domain::Domain(Domain&& mov) noexcept :
+Domain::Domain(Domain && mov) noexcept :
     TriggerType(std::move(mov)),
     exception::Logic(std::move(mov))
 {}
 
-const char* Domain::Message() const noexcept
+const typename Domain::CharType * Domain::Message() const noexcept
 {
     return exception::Logic::Message();
 }
 
-typename Domain::OutputValueType& 
-Domain::Output(OutputValueType& out) const noexcept
+typename Domain::OutputValueType & 
+Domain::Output(OutputValueType & out) const noexcept
 {
     return exception::Logic::Output(out);
 }
@@ -92,48 +107,7 @@ Domain::Output(OutputValueType& out) const noexcept
 
 #ifdef USING_STANDARD_EXCEPTION
 
-class Domain : public std::domain_error
-{
-public:
-    Domain(const char* file, const std::size_t& line) noexcept;
-    Domain(const char * what_arg, const char* file, 
-        const std::size_t& line) noexcept;
-public:
-    Domain(const Domain& cpy) noexcept;
-    Domain(Domain&& mov) noexcept;
-public:
-    virtual ~Domain() noexcept;
-public:
-    Domain& operator=(const Domain&) = delete;
-    Domain& operator=(Domain&)& = delete;
-public:
-    virtual const char * what() const noexcept;
-};
-
-Domain::Domain(const char* file, const std::size_t& line) noexcept :
-    std::domain_error("Domain Logic Exception")
-{}
-
-Domain::Domain(const char * what_arg, const char* file, 
-    const std::size_t& line) noexcept :
-        std::domain_error(what_arg)
-{}
-
-Domain::Domain(const Domain& cpy) noexcept :
-    std::domain_error(cpy)
-{}
-
-Domain::Domain(Domain&& mov) noexcept :
-    std::domain_error(mov)
-{}
-
-Domain::~Domain() noexcept
-{}
-
-const char * Domain::what() const noexcept
-{
-    return std::domain_error::what();
-}
+using Domain = std::domain_error;
 
 #endif //!USING_STANDARD_EXCEPTION
 
@@ -148,18 +122,17 @@ namespace id
 
 #ifndef USING_STANDARD_EXCEPTION
 
-
-constexpr Identification Get(const exception::logic::Domain& e)
+constexpr Identification Get(const exception::logic::Domain & e)
 {
     return Identification(constant::error::logic_domain_id);
-};
+}
 
 #endif //!USING_STANDARD_EXCEPTION
 
-constexpr Identification Get(const std::domain_error& e)
+constexpr Identification Get(const std::domain_error & e)
 {
     return Standard(constant::error::logic_domain_id);
-};
+}
 
 #endif //!USING_EXCEPTION
 
