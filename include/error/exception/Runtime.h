c6ba2efb0d27454f0077e2344657c6b10e8b8569
AnantaYudica <nta040689@gmail.com>
2018-12-10 21:11:38 +0700

    Update dependencies Update basic::error::exception::Runtime Update overloading function basic::error::id::Get

--- include/error/exception/Runtime.h
+++ include/error/exception/Runtime.h
@@ -5,6 +5,10 @@
 #include "../Identification.h"
 #include "../id/Get.h"
 #include "../id/Standard.h"
+#include "../defn/type/Char.h"
+#include "../defn/type/Output.h"
+#include "../output/Operator.h"
+#include "../msg/String.h"
 #include "../../constant/error/Identification.h"
 
 #include <stdexcept>
@@ -18,43 +22,95 @@ namespace exception
 {
 
 #ifdef USING_BASIC_ERROR_EXCEPTION
+
 class Runtime : public error::Exception
 {
 public:
-    typedef typename error::Exception::ErrorType ErrorType;
+    typedef typename error::Exception::TriggerType TriggerType;
+public:
+    typedef defn::type::Char CharType;
+    typedef defn::type::Output OutputValueType;
+public:
+    typedef msg::String StringType;
+private:
+    StringType m_message;
 protected:
-    Runtime();
+    Runtime() noexcept;
+
+#ifdef USING_BASIC_ERROR_FILE_AND_LINE
+
+protected:
+    Runtime(const CharType * message) noexcept;
 public:
-    Runtime(const char* file, const std::size_t& line);
+    Runtime(const CharType * message, const char* file, 
+        const std::size_t& line) noexcept;
+
+#else
+
 public:
-    Runtime(const Runtime& cpy);
-    Runtime(Runtime&& mov);
+    Runtime(const CharType * message) noexcept;
+
+#endif //!USING_BASIC_ERROR_FILE_AND_LINE
+
 public:
-    ~Runtime();
+    Runtime(const Runtime & cpy) noexcept;
+    Runtime(Runtime && mov) noexcept;
 public:
-    Runtime& operator=(const Runtime&) = delete;
-    Runtime& operator=(Runtime&&) = delete;
+    Runtime & operator=(const Runtime &) = delete;
+    Runtime & operator=(Runtime &&) = delete;
+public:
+    virtual const CharType * Message() const noexcept;
+protected:
+    virtual OutputValueType& Output(OutputValueType& out) const noexcept;
 };
 
-Runtime::Runtime()
+Runtime::Runtime() noexcept :
+    TriggerType(constant::error::runtime_id),
+    m_message("Runtime Exception")
 {}
 
-Runtime::Runtime(const char* file, const std::size_t& line) :
-    ErrorType(constant::error::runtime_id, file, line)
+Runtime::Runtime(const CharType * message) noexcept :
+    TriggerType(constant::error::runtime_id),
+    m_message(message)
 {}
 
-Runtime::Runtime(const Runtime& cpy) :
+#ifdef USING_BASIC_ERROR_FILE_AND_LINE
+
+Runtime::Runtime(const CharType * message, const char* file, 
+    const std::size_t& line) noexcept :
+        TriggerType(constant::error::runtime_id, file, line),
+        m_message(message)
+{}
+
+#endif //!USING_BASIC_ERROR_FILE_AND_LINE
+
+Runtime::Runtime(const Runtime& cpy) noexcept :
+    TriggerType(cpy),
     error::Exception(cpy)
 {}
 
-Runtime::Runtime(Runtime&& mov) :
+Runtime::Runtime(Runtime&& mov) noexcept :
+    TriggerType(std::move(mov)),
     error::Exception(std::move(mov))
 {}
 
-Runtime::~Runtime()
-{}
+const typename Runtime::CharType * Runtime::Message() const noexcept
+{
+    if (m_message)
+        return m_message.Value();
+    return "Runtime Exception";
+}
 
-#elif USING_STANDARD_EXCEPTION
+typename Runtime::OutputValueType& 
+Runtime::Output(OutputValueType& out) const noexcept
+{
+    error::Exception::Output(out);
+    return out;
+}
+
+#endif //!USING_BASIC_ERROR_EXCEPTION
+
+#ifdef USING_STANDARD_EXCEPTION
 
 typedef std::runtime_error Runtime;
 
@@ -62,19 +118,21 @@ typedef std::runtime_error Runtime;
 
 } //!exception
 
-#ifndef USING_STANDARD_EXCEPTION
-
-constexpr Identification id::Get(const exception::Runtime& e)
+namespace id
 {
-    return Identification(constant::error::runtime_id);
+
+#ifdef USING_EXCEPTION
+
+template<typename TTagError = tag::Trigger>
+typename enable_if::tag::Trigger<TTagError>::Type 
+Get(const std::runtime_error& e) noexcept
+{
+    return Standard(constant::error::runtime_id);
 };
 
-#endif
+#endif //!USING_EXCEPTION
 
-constexpr Identification id::Get(const std::runtime_error& e)
-{
-    return id::Standard(Identification(constant::error::runtime_id));
-};
+} //!id
 
 } //!error
 
