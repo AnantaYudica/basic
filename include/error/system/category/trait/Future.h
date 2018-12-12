185173d488962145281e79fde3d3baf4d5efa00f
AnantaYudica <nta040689@gmail.com>
2018-12-12 20:57:54 +0700

    Update class basic::error::system::category::trait::Future

--- include/error/system/category/trait/Future.h
+++ include/error/system/category/trait/Future.h
@@ -2,7 +2,6 @@
 #define BASIC_ERROR_SYSTEM_CATEGORY_TRAIT_FUTURE_H_
 
 #include "../../../../constant/error/system/Category.h"
-
 #include "../../../defn/type/Char.h"
 #include "../../../defn/type/system/category/Value.h"
 #include "../../../defn/type/system/code/Value.h"
@@ -37,42 +36,42 @@ public:
 public:
     static Future Instance() noexcept;
 public:
-    Future() noexcept = default;
-    Future(const Future&) noexcept = default;
-    Future(Future&&) noexcept = default;
+    inline Future() noexcept = default;
+    inline Future(const Future&) noexcept = default;
+    inline Future(Future&&) noexcept = default;
+public:
+    template<typename TCondition, typename TCode>
+    inline TCondition DefaultCondition(const TCode& code) const noexcept;
 public:
     template<typename TCode>
-    ConditionEnumType DefaultCondition(const TCode& code) const noexcept;
-public:
-    template<typename TCode>
-    bool Equivalent(const TCode& code, 
+    inline bool Equivalent(const TCode& code, 
         const ConditionValueType& cond) const noexcept;
     template<typename TCondition>
-    bool Equivalent(const CodeValueType& code, 
+    inline bool Equivalent(const CodeValueType& code, 
         const TCondition& cond) const noexcept;
 public:
     template<typename TValue>
-    StringType Message(const TValue& val) const noexcept;
+    inline StringType Message(const TValue& val) const noexcept;
 public:
-    const CharType * Name() const noexcept;
+    inline const CharType * Name() const noexcept;
 public:
-    CategoryValueType Value() const noexcept;
+    inline CategoryValueType Value() const noexcept;
 };
 
-Future Future::Instance() noexcept
+inline Future Future::Instance() noexcept
 {
     return {};
 }
 
-template<typename TCode>
-typename Future::ConditionEnumType 
-Future::DefaultCondition(const TCode& code) const noexcept
+template<typename TCondition, typename TCode>
+inline TCondition Future::DefaultCondition(const TCode& code) const noexcept
 {
-    return std::future_category().default_error_condition(code.Value());
+    return {std::future_category().
+        default_error_condition(code.Value()).value()};
 }
 
 template<typename TCode>
-bool Future::Equivalent(const TCode& code, 
+inline bool Future::Equivalent(const TCode& code, 
     const ConditionValueType& cond) const noexcept
 {
     return std::future_category().equivalent(std::error_code(code.Value(), 
@@ -80,7 +79,7 @@ bool Future::Equivalent(const TCode& code,
 }
 
 template<typename TCondition>
-bool Future::Equivalent(const CodeValueType& code, 
+inline bool Future::Equivalent(const CodeValueType& code, 
     const TCondition& cond) const noexcept
 {
     return std::future_category().equivalent(code, 
@@ -88,18 +87,18 @@ bool Future::Equivalent(const CodeValueType& code,
 }
 
 template<typename TValue>
-typename Future::StringType 
+inline typename Future::StringType 
 Future::Message(const TValue& val) const noexcept
 {
     return {std::future_category().message(val.Value())};
 }
 
-const typename Future::CharType * Future::Name() const noexcept
+inline const typename Future::CharType * Future::Name() const noexcept
 {
     return std::future_category().name();
 }
 
-typename Future::CategoryValueType Future::Value() const noexcept
+inline typename Future::CategoryValueType Future::Value() const noexcept
 {
     return constant::error::system::future_category;
 }
