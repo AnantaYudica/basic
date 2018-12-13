65c69ce096e5597c0093bf54adc6cdd854cde6b1
AnantaYudica <nta040689@gmail.com>
2018-12-13 15:44:22 +0700

    Add class interface forward declaration of basic::error::system::CategoryI

---
+++ include/error/system/Category.i.d.h
@@ -0,0 +1,19 @@
+#ifndef BASIC_ERROR_SYSTEM_CATEGORY_I_D_H_
+#define BASIC_ERROR_SYSTEM_CATEGORY_I_D_H_
+
+namespace basic
+{
+namespace error
+{
+namespace system
+{
+
+class CategoryI;
+
+} //!system
+
+} //!error
+
+} //!basic
+
+#endif //!BASIC_ERROR_SYSTEM_CATEGORY_I_D_H_
