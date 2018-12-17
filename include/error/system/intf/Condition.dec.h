d72e98b751ddcabea8360751127109b5ee245d4f
AnantaYudica <nta040689@gmail.com>
2018-12-17 16:40:49 +0700

    Add declaration class Interface basic::error::system::intf::Condition

---
+++ include/error/system/intf/Condition.dec.h
@@ -0,0 +1,23 @@
+#ifndef BASIC_ERROR_SYSTEM_INTF_CONDITION_DEC_H_
+#define BASIC_ERROR_SYSTEM_INTF_CONDITION_DEC_H_
+
+namespace basic
+{
+namespace error
+{
+namespace system
+{
+namespace intf
+{
+
+class Condition;
+
+} //!intf
+
+} //!basic
+
+} //!error
+
+} //!system 
+
+#endif //!BASIC_ERROR_SYSTEM_INTF_CONDITION_FDC_H_
