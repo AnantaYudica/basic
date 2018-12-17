e18a4122cde3a734c7bd3367240b8b3683259780
AnantaYudica <nta040689@gmail.com>
2018-12-17 18:07:33 +0700

    Add declaration class basic::error::system::Condition

---
+++ include/error/system/Condition.dec.h
@@ -0,0 +1,19 @@
+#ifndef BASIC_ERROR_SYSTEM_CONDITION_DEC_H_
+#define BASIC_ERROR_SYSTEM_CONDITION_DEC_H_
+
+namespace basic
+{
+namespace error
+{
+namespace system
+{
+
+class Condition;
+
+} //!system
+
+} //!error
+
+} //!basic
+
+#endif //!BASIC_ERROR_SYSTEM_CONDITION_DEC_H_
