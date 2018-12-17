715d60df817c07331aece4882766a5f3ff3530b1
AnantaYudica <nta040689@gmail.com>
2018-12-17 15:39:55 +0700

    Add declaration class Interface basic::error::system::intf::Code

---
+++ include/error/system/intf/Code.dec.h
@@ -0,0 +1,23 @@
+#ifndef BASIC_ERROR_SYSTEM_INTF_CODE_DEC_H_
+#define BASIC_ERROR_SYSTEM_INTF_CODE_DEC_H_
+
+namespace basic
+{
+namespace error
+{
+namespace system
+{
+namespace intf
+{
+
+class Code;
+
+} //!intf
+
+} //!basic
+
+} //!error
+
+} //!system 
+
+#endif //!BASIC_ERROR_SYSTEM_INTF_CODE_DEC_H_
