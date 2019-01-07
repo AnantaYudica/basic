17980bd4119f1560efcf195733228fd1642a3a78
AnantaYudica <nta040689@gmail.com>
2019-01-07 15:42:50 +0700

    Moved file from Condition.dec.h to Condition.decl.h

---
+++ include/error/system/intf/Condition.decl.h
@@ -0,0 +1,23 @@
+#ifndef BASIC_ERROR_SYSTEM_INTF_CONDITION_H_DECL_
+#define BASIC_ERROR_SYSTEM_INTF_CONDITION_H_DECL_
+
+namespace basic
+{
+namespace error
+{
+namespace system
+{
+namespace intf
+{
+
+class Condition;
+
+} //!intf
+
+} //!basic
+
+} //!error
+
+} //!system 
+
+#endif //!BASIC_ERROR_SYSTEM_INTF_CONDITION_H_DECL_
