66b8b77d83e346e03dce9c57811a2c6d8fc1a8c0
AnantaYudica <nta040689@gmail.com>
2019-01-07 16:13:13 +0700

    Moved file from Condition.dec.h to Condition.decl.h

---
+++ include/error/system/Condition.decl.h
@@ -0,0 +1,19 @@
+#ifndef BASIC_ERROR_SYSTEM_CONDITION_H_DECL_
+#define BASIC_ERROR_SYSTEM_CONDITION_H_DECL_
+
+namespace basic
+{
+namespace error
+{
+namespace system
+{
+
+class Condition;
+
+} //!system
+
+} //!error
+
+} //!basic
+
+#endif //!BASIC_ERROR_SYSTEM_CONDITION_H_DECL_
