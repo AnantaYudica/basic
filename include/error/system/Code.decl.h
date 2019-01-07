9a4443f3db7fca70ff4934f6a8b29ce00dbf05c5
AnantaYudica <nta040689@gmail.com>
2019-01-07 16:06:32 +0700

    Moved file from Code.dec.h to Code.decl.h

---
+++ include/error/system/Code.decl.h
@@ -0,0 +1,19 @@
+#ifndef BASIC_ERROR_SYSTEM_CODE_H_DECL_
+#define BASIC_ERROR_SYSTEM_CODE_H_DECL_
+
+namespace basic
+{
+namespace error
+{
+namespace system
+{
+
+class Code;
+
+} //!system
+
+} //!error
+
+} //!basic
+
+#endif //!BASIC_ERROR_SYSTEM_CODE_H_DECL_
