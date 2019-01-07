bc730c2a418780196e12d872f977285bbd381b1a
AnantaYudica <nta040689@gmail.com>
2019-01-07 16:53:27 +0700

    Added declaration class

---
+++ include/error/system/tmpl/Condition.decl.h
@@ -0,0 +1,24 @@
+#ifndef BASIC_ERROR_SYSTEM_TMPL_CONDITION_H_DECL_
+#define BASIC_ERROR_SYSTEM_TMPL_CONDITION_H_DECL_
+
+namespace basic
+{
+namespace error
+{
+namespace system
+{
+namespace tmpl
+{
+
+template<typename TCategoryTrait>
+class Condition;
+
+} //!tmpl
+
+} //!system
+
+} //!error
+
+} //!basic
+
+#endif //!BASIC_ERROR_SYSTEM_TMPL_CONDITION_H_DECL_
