b658a69d1ea78589d0aba0d0fc6003a8f4af5939
AnantaYudica <nta040689@gmail.com>
2019-01-11 11:51:42 +0700

    Added class _ToConditionValue Added alias type ToConditionValue

---
+++ include/error/system/tmpl/category/has/mmbr/func/ToConditionValue.h
@@ -0,0 +1,86 @@
+#ifndef BASIC_ERROR_SYSTEM_TMPL_CATEGORY_HAS_MMBR_FUNC_TOCONDITIONVALUE_H_
+#define BASIC_ERROR_SYSTEM_TMPL_CATEGORY_HAS_MMBR_FUNC_TOCONDITIONVALUE_H_
+
+#include "../../../../../../macro/HasMemberFunction.h"
+#include "../../../../../defn/type/system/condition/Value.h"
+
+namespace _helper
+{
+namespace _basic
+{
+namespace _error
+{
+namespace _system
+{
+namespace _tmpl
+{
+namespace _category
+{
+namespace _has
+{
+namespace _mmbr
+{
+namespace _func
+{
+
+__DEFINE_HAS_MMBR_FUNC_(_ToConditionValue, __ToConditionValue, TCategoryTrait, 
+    TRet, TArgs, const,,, T_NAME_,,,,, PROTO_FUNC_T_NAME_,, 
+    PROTO_FUNC_T_RET_NAME_,, ToConditionValue);
+
+} //!_func
+
+} //!_mmbr
+
+} //!_has
+
+} //!_category
+
+} //!_tmpl
+
+} //!_system
+
+} //!_error
+
+} //!_basic
+
+} //!_helper
+
+namespace basic
+{
+namespace error
+{
+namespace system
+{
+namespace tmpl
+{
+namespace category
+{
+namespace has
+{
+namespace mmbr
+{
+namespace func
+{
+
+template<typename TCategoryTrait, typename TConditionEnum>
+using ToConditionValue = _helper::_basic::_error::_system::_tmpl::_category::
+    _has::_mmbr::_func::_ToConditionValue<TCategoryTrait, 
+    defn::type::system::condition::Value, TConditionEnum>;
+
+} //!func
+
+} //!mmbr
+
+} //!has
+
+} //!category
+
+} //!tmpl
+
+} //!system
+
+} //!error
+
+} //!basic
+
+#endif //!BASIC_ERROR_SYSTEM_TMPL_CATEGORY_HAS_MMBR_FUNC_TOCONDITIONVALUE_H_
