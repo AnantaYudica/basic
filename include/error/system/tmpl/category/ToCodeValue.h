8d3b9f7b36d8d4ebf04bb7f8149a59127d70ed62
AnantaYudica <nta040689@gmail.com>
2019-01-11 11:59:50 +0700

    Added function ToCodeValue

---
+++ include/error/system/tmpl/category/ToCodeValue.h
@@ -0,0 +1,50 @@
+#ifndef BASIC_ERROR_SYSTEM_TMPL_CATEGORY_TOCODEVALUE_H_
+#define BASIC_ERROR_SYSTEM_TMPL_CATEGORY_TOCODEVALUE_H_
+
+#include "has/mmbr/func/ToCodeValue.h"
+#include "../../defn/type/system/code/Value.h"
+
+#include <type_traits>
+
+namespace basic
+{
+namespace error
+{
+namespace system
+{
+namespace tmpl
+{
+namespace category
+{
+
+template<typename TCategoryTrait, typename TCodeEnum>
+inline typename std::enable_if<has::mmbr::func::
+    ToCodeValue<TCategoryTrait, TCodeEnum>::Value, 
+    error::defn::type::system::code::Value>::type 
+DefaultCodeValue(const TCategoryTrait & category_trait, 
+    const TCodeEnum & code) noexcept
+{
+    return category_trait.ToCodeValue(code);
+}
+
+template<typename TCategoryTrait, typename TCodeEnum>
+inline typename std::enable_if<!has::mmbr::func::
+    ToCodeValue<TCategoryTrait, TCodeEnum>::Value, 
+    error::defn::type::system::code::Value>::type 
+DefaultCodeValue(const TCategoryTrait & category_trait, 
+    const TCodeEnum & code) noexcept
+{
+    return static_cast<error::defn::type::system::code::Value>(code);
+}
+
+} //!category
+
+} //!tmpl
+
+} //!system
+
+} //!error
+
+} //!basic
+
+#endif //!BASIC_ERROR_SYSTEM_TMPL_CATEGORY_TOCODEVALUE_H_
