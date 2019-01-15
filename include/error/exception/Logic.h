3a4f5d60e7f840ae43f73c50597ff04b6276c360
AnantaYudica <nta040689@gmail.com>
2019-01-15 15:20:18 +0700

    Added inline specifier Updated name alias type member Removed function member Output Added function member operator>>

--- include/error/exception/Logic.h
+++ include/error/exception/Logic.h
@@ -29,56 +29,57 @@ public:
     typedef typename error::Exception::TriggerType TriggerType;
 public:
     typedef defn::type::Char CharType;
-    typedef defn::type::Output OutputValueType;
+    typedef defn::type::Output OutputType;
 public:
     typedef msg::String StringType;
 private:
     StringType m_message;
 protected:
-    Logic() noexcept;
+    inline Logic() noexcept;
 
 #ifdef USING_BASIC_ERROR_FILE_AND_LINE
 
 protected:
-    Logic(const CharType * message) noexcept;
+    inline Logic(const CharType * message) noexcept;
 public:
-    Logic(const CharType * message, const char * file, 
+    inline Logic(const CharType * message, const char * file, 
         const std::size_t & line) noexcept;
 
 #else
 
 public:
-    Logic(const CharType * message) noexcept;
+    inline Logic(const CharType * message) noexcept;
 
 #endif //!USING_BASIC_ERROR_FILE_AND_LINE
 
 protected:
-    Logic(StringType && message) noexcept;
+    inline Logic(StringType && message) noexcept;
 public:
-    Logic(const Logic & cpy) noexcept;
-    Logic(Logic && mov) noexcept;
+    inline Logic(const Logic & cpy) noexcept;
+    inline Logic(Logic && mov) noexcept;
 public:
-    Logic & operator=(const Logic &) = delete;
-    Logic & operator=(Logic &&) = delete;
+    inline Logic & operator=(const Logic &) = delete;
+    inline Logic & operator=(Logic &&) = delete;
 public:
-    virtual const CharType * Message() const noexcept;
+    virtual inline const CharType * Message() const noexcept;
 protected:
-    virtual OutputValueType & Output(OutputValueType & out) const noexcept;
+    virtual inline const Logic & 
+    operator>>(OutputType & out) const noexcept;
 };
 
-Logic::Logic() noexcept :
+inline Logic::Logic() noexcept :
     TriggerType(constant::error::logic_id),
     m_message("Logic Exception")
 {}
 
-Logic::Logic(const CharType * message) noexcept :
+inline Logic::Logic(const CharType * message) noexcept :
     TriggerType(constant::error::logic_id),
     m_message(message)
 {}
 
 #ifdef USING_BASIC_ERROR_FILE_AND_LINE
 
-Logic::Logic(const CharType * message, const char * file, 
+inline Logic::Logic(const CharType * message, const char * file, 
     const std::size_t & line) noexcept :
         TriggerType(constant::error::logic_id, file, line),
         m_message(message)
@@ -86,35 +87,35 @@ Logic::Logic(const CharType * message, const char * file,
 
 #endif //!USING_BASIC_ERROR_FILE_AND_LINE
 
-Logic::Logic(StringType && message) noexcept :
+inline Logic::Logic(StringType && message) noexcept :
     TriggerType(constant::error::logic_id),
     m_message(std::move(message))
 {}
 
-Logic::Logic(const Logic & cpy) noexcept :
+inline Logic::Logic(const Logic & cpy) noexcept :
     TriggerType(cpy),
     m_message(cpy.m_message),
     error::Exception(cpy)
 {}
 
-Logic::Logic(Logic && mov) noexcept :
+inline Logic::Logic(Logic && mov) noexcept :
     TriggerType(std::move(mov)),
     m_message(std::move(mov.m_message)),
     error::Exception(std::move(mov))
 {}
 
-const typename Logic::CharType * Logic::Message() const noexcept
+inline const typename Logic::CharType * Logic::Message() const noexcept
 {
     if (m_message)
         return m_message.Value();
     return "Logic Exception";
 }
 
-typename Logic::OutputValueType & 
-Logic::Output(OutputValueType & out) const noexcept
+inline const Logic & 
+Logic::operator>>(OutputType & out) const noexcept
 {
-    error::Exception::Output(out);
-    return out;
+    error::Exception::operator>>(out);
+    return *this;
 }
 
 #endif //!USING_BASIC_ERROR_EXCEPTION
@@ -133,7 +134,7 @@ namespace id
 #ifdef USING_EXCEPTION
 
 template<typename TTagError = tag::Trigger>
-typename enable_if::tag::Trigger<TTagError>::Type 
+inline typename enable_if::tag::Trigger<TTagError>::Type 
 Get(const std::logic_error & e) noexcept
 {
     return Standard(constant::error::logic_id);
