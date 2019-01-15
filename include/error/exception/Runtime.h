d0109714a52c56583edd890dc371f354f095c63d
AnantaYudica <nta040689@gmail.com>
2019-01-15 15:51:14 +0700

    Added inline specifier Updated name alias type member Removed function member Output Added function member operator>>

--- include/error/exception/Runtime.h
+++ include/error/exception/Runtime.h
@@ -29,56 +29,57 @@ public:
     typedef typename error::Exception::TriggerType TriggerType;
 public:
     typedef defn::type::Char CharType;
-    typedef defn::type::Output OutputValueType;
+    typedef defn::type::Output OutputType;
 public:
     typedef msg::String StringType;
 private:
     StringType m_message;
 protected:
-    Runtime() noexcept;
+    inline Runtime() noexcept;
 
 #ifdef USING_BASIC_ERROR_FILE_AND_LINE
 
 protected:
-    Runtime(const CharType * message) noexcept;
+    inline Runtime(const CharType * message) noexcept;
 public:
-    Runtime(const CharType * message, const char* file, 
+    inline Runtime(const CharType * message, const char* file, 
         const std::size_t& line) noexcept;
 
 #else
 
 public:
-    Runtime(const CharType * message) noexcept;
+    inline Runtime(const CharType * message) noexcept;
 
 #endif //!USING_BASIC_ERROR_FILE_AND_LINE
 
 protected:
-    Runtime(StringType && message) noexcept;
+    inline Runtime(StringType && message) noexcept;
 public:
-    Runtime(const Runtime & cpy) noexcept;
-    Runtime(Runtime && mov) noexcept;
+    inline Runtime(const Runtime & cpy) noexcept;
+    inline Runtime(Runtime && mov) noexcept;
 public:
-    Runtime & operator=(const Runtime &) = delete;
-    Runtime & operator=(Runtime &&) = delete;
+    inline Runtime & operator=(const Runtime &) = delete;
+    inline Runtime & operator=(Runtime &&) = delete;
 public:
-    virtual const CharType * Message() const noexcept;
+    virtual inline const CharType * Message() const noexcept;
 protected:
-    virtual OutputValueType & Output(OutputValueType& out) const noexcept;
+    virtual inline const Runtime & 
+    operator>>(OutputType & out) const noexcept;
 };
 
-Runtime::Runtime() noexcept :
+inline Runtime::Runtime() noexcept :
     TriggerType(constant::error::runtime_id),
     m_message("Runtime Exception")
 {}
 
-Runtime::Runtime(const CharType * message) noexcept :
+inline Runtime::Runtime(const CharType * message) noexcept :
     TriggerType(constant::error::runtime_id),
     m_message(message)
 {}
 
 #ifdef USING_BASIC_ERROR_FILE_AND_LINE
 
-Runtime::Runtime(const CharType * message, const char* file, 
+inline Runtime::Runtime(const CharType * message, const char* file, 
     const std::size_t& line) noexcept :
         TriggerType(constant::error::runtime_id, file, line),
         m_message(message)
@@ -86,33 +87,32 @@ Runtime::Runtime(const CharType * message, const char* file,
 
 #endif //!USING_BASIC_ERROR_FILE_AND_LINE
 
-Runtime::Runtime(StringType && message) noexcept :
+inline Runtime::Runtime(StringType && message) noexcept :
     TriggerType(constant::error::runtime_id),
     m_message(std::move(message))
 {}
 
-Runtime::Runtime(const Runtime & cpy) noexcept :
+inline Runtime::Runtime(const Runtime & cpy) noexcept :
     TriggerType(cpy),
     error::Exception(cpy)
 {}
 
-Runtime::Runtime(Runtime && mov) noexcept :
+inline Runtime::Runtime(Runtime && mov) noexcept :
     TriggerType(std::move(mov)),
     error::Exception(std::move(mov))
 {}
 
-const typename Runtime::CharType * Runtime::Message() const noexcept
+inline const typename Runtime::CharType * Runtime::Message() const noexcept
 {
     if (m_message)
         return m_message.Value();
     return "Runtime Exception";
 }
 
-typename Runtime::OutputValueType & 
-Runtime::Output(OutputValueType & out) const noexcept
+inline const Runtime & Runtime::operator>>(OutputType & out) const noexcept
 {
-    error::Exception::Output(out);
-    return out;
+    error::Exception::operator>>(out);
+    return *this;
 }
 
 #endif //!USING_BASIC_ERROR_EXCEPTION
@@ -131,7 +131,7 @@ namespace id
 #ifdef USING_EXCEPTION
 
 template<typename TTagError = tag::Trigger>
-typename enable_if::tag::Trigger<TTagError>::Type 
+inline typename enable_if::tag::Trigger<TTagError>::Type 
 Get(const std::runtime_error & e) noexcept
 {
     return Standard(constant::error::runtime_id);
