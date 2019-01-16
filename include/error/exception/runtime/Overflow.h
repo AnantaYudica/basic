ca4a5f42ba0b1049b1b8f97315bf11194529aadf
AnantaYudica <nta040689@gmail.com>
2019-01-16 13:59:03 +0700

    Added inline specifier Updated name alias type member Removed function member Output Added function member operator>>

--- include/error/exception/runtime/Overflow.h
+++ include/error/exception/runtime/Overflow.h
@@ -30,50 +30,51 @@ public:
     typedef typename error::Exception::TriggerType TriggerType;
 public:
     typedef defn::type::Char CharType;
-    typedef defn::type::Output OutputValueType;
+    typedef defn::type::Output OutputType;
 protected:
-    Overflow() noexcept;
+    inline Overflow() noexcept;
 
 #ifdef USING_BASIC_ERROR_FILE_AND_LINE
 
 protected:
-    Overflow(const CharType * message) noexcept;
+    inline Overflow(const CharType * message) noexcept;
 public:
-    Overflow(const CharType * message, const char * file, 
+    inline Overflow(const CharType * message, const char * file, 
         const std::size_t & line) noexcept;
 
 #else
 
 public:
-    Overflow(const CharType * message) noexcept;
+    inline Overflow(const CharType * message) noexcept;
 
 #endif //!USING_BASIC_ERROR_FILE_AND_LINE
 
 public:
-    Overflow(const Overflow & cpy) noexcept;
-    Overflow(Overflow && mov) noexcept;
+    inline Overflow(const Overflow & cpy) noexcept;
+    inline Overflow(Overflow && mov) noexcept;
 public:
-    Overflow & operator=(const Overflow &) = delete;
-    Overflow & operator=(Overflow &&) = delete;
+    inline Overflow & operator=(const Overflow &) = delete;
+    inline Overflow & operator=(Overflow &&) = delete;
 public:
-    virtual const CharType * Message() const noexcept;
+    virtual inline const CharType * Message() const noexcept;
 protected:
-    virtual OutputValueType & Output(OutputValueType & out) const noexcept;
+    virtual inline const error::intf::Output & 
+    operator>>(OutputType & out) const noexcept;
 };
 
-Overflow::Overflow() noexcept :
+inline Overflow::Overflow() noexcept :
     TriggerType(constant::error::runtime_overflow_id),
     exception::Runtime("Overflow Runtime Exception")
 {}
 
-Overflow::Overflow(const CharType * message) noexcept :
+inline Overflow::Overflow(const CharType * message) noexcept :
     TriggerType(constant::error::runtime_overflow_id),
     exception::Runtime(message)
 {}
 
 #ifdef USING_BASIC_ERROR_FILE_AND_LINE
 
-Overflow::Overflow(const CharType * message, const char * file, 
+inline Overflow::Overflow(const CharType * message, const char * file, 
     const std::size_t & line) noexcept :
         TriggerType(constant::error::runtime_overflow_id, file, line),
         exception::Runtime(message)
@@ -81,25 +82,26 @@ Overflow::Overflow(const CharType * message, const char * file,
 
 #endif //!USING_BASIC_ERROR_FILE_AND_LINE
 
-Overflow::Overflow(const Overflow & cpy) noexcept :
+inline Overflow::Overflow(const Overflow & cpy) noexcept :
     TriggerType(cpy),
     exception::Runtime(cpy)
 {}
 
-Overflow::Overflow(Overflow && mov) noexcept :
+inline Overflow::Overflow(Overflow && mov) noexcept :
     TriggerType(std::move(mov)),
     exception::Runtime(std::move(mov))
 {}
 
-const typename Overflow::CharType * Overflow::Message() const noexcept
+inline const typename Overflow::CharType * Overflow::Message() const noexcept
 {
     return exception::Runtime::Message();
 }
 
-typename Overflow::OutputValueType & 
-Overflow::Output(OutputValueType & out) const noexcept
+inline const error::intf::Output & 
+Overflow::operator>>(OutputType & out) const noexcept
 {
-    return exception::Runtime::Output(out);
+    exception::Runtime::operator>>(out);
+    return *this;
 }
 
 #endif //!USING_BASIC_ERROR_EXCEPTION
@@ -118,7 +120,7 @@ namespace id
 {
 
 template<typename TTagError = tag::Trigger>
-typename enable_if::tag::Trigger<TTagError>::Type 
+inline typename enable_if::tag::Trigger<TTagError>::Type 
 Get(const std::overflow_error & e)
 {
     return Standard(constant::error::runtime_overflow_id);
