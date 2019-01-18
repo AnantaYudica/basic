576d4431bc9e651efff85cf689f73c2bed24c55e
AnantaYudica <nta040689@gmail.com>
2019-01-18 16:28:51 +0700

    Updated dependencies Updated functions

--- include/error/system/tmpl/category/DefaultCode.h
+++ include/error/system/tmpl/category/DefaultCode.h
@@ -1,7 +1,11 @@
 #ifndef BASIC_ERROR_SYSTEM_TMPL_CATEGORY_DEFAULTCODE_H_
 #define BASIC_ERROR_SYSTEM_TMPL_CATEGORY_DEFAULTCODE_H_
 
+#include "../Category.defn.h"
+
 #include "has/mmbr/func/DefaultCode.h"
+#include "DefaultCodeValue.h"
+
 #include "../../Code.h"
 
 #include <type_traits>
@@ -20,18 +24,23 @@ namespace category
 
 template<typename TCategoryTrait>
 typename std::enable_if<has::mmbr::func::DefaultCode<TCategoryTrait,
-    system::Code>::Value, system::Code>::type  
-DefaultCode(const TCategoryTrait& category_trait) noexcept
+    system::Code, tmpl::Category<TCategoryTrait>>::Value, 
+    system::Code>::type  
+DefaultCode(const TCategoryTrait& category_trait, 
+    const tmpl::Category<TCategoryTrait> & category_instance) noexcept
 {
-    return std::move(category_trait.template DefaultCode<system::Code>());
+    return std::move(category_trait.template 
+        DefaultCode<system::Code>(category_instance));
 }
 
 template<typename TCategoryTrait>
 typename std::enable_if<!has::mmbr::func::DefaultCode<TCategoryTrait,
-    system::Code>::Value, system::Code>::type  
-DefaultCode(const TCategoryTrait& category_trait) noexcept
+    system::Code, const tmpl::Category<TCategoryTrait> &>::Value, 
+    system::Code>::type  
+DefaultCode(const TCategoryTrait& category_trait, 
+    const tmpl::Category<TCategoryTrait> & category_instance) noexcept
 {
-    return {};
+    return {DefaultCodeValue(category_trait), category_instance};
 }
 
 } //!category
--- include/error/system/tmpl/category/DefaultCondition.h
+++ include/error/system/tmpl/category/DefaultCondition.h
@@ -1,6 +1,8 @@
 #ifndef BASIC_ERROR_SYSTEM_TMPL_CATEGORY_DEFAULTCONDITION_H_
 #define BASIC_ERROR_SYSTEM_TMPL_CATEGORY_DEFAULTCONDITION_H_
 
+#include "../Category.defn.h"
+
 #include "has/mmbr/func/DefaultCondition.h"
 #include "../../Condition.h"
 
@@ -20,21 +22,23 @@ namespace category
 
 template<typename TCategoryTrait, typename TCode>
 typename std::enable_if<has::mmbr::func::DefaultCondition<TCategoryTrait,
-    system::Condition, TCode>::Value, system::Condition>::type  
-DefaultCondition(const TCategoryTrait & category_trait, const TCode & code) 
-    noexcept
+    system::Condition, TCode, tmpl::Category<TCategoryTrait>>::Value, 
+    system::Condition>::type  
+DefaultCondition(const TCategoryTrait & category_trait, const TCode & code,
+    const tmpl::Category<TCategoryTrait> & category_instance) noexcept
 {
     return std::move(category_trait.template 
-        DefaultCondition<system::Condition>(code));
+        DefaultCondition<system::Condition>(code, category_instance));
 }
 
 template<typename TCategoryTrait, typename TCode>
 typename std::enable_if<!has::mmbr::func::DefaultCondition<TCategoryTrait,
-    system::Condition, TCode>::Value, system::Condition>::type  
-DefaultCondition(const TCategoryTrait & category_trait, const TCode & code) 
-    noexcept
+    system::Condition, TCode, tmpl::Category<TCategoryTrait>>::Value, 
+    system::Condition>::type  
+DefaultCondition(const TCategoryTrait & category_trait, const TCode & code,
+    const tmpl::Category<TCategoryTrait> & category_instance) noexcept
 {
-    return {code};
+    return {code, category_instance};
 }
 
 } //!category
