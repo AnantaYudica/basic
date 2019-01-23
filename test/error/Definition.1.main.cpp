5dd43a5aafe86ab891f9865c4f8469d7f574d2d6
AnantaYudica <nta040689@gmail.com>
2019-01-23 23:42:18 +0700

    Updated dependencies Add and Updated alias type Added tests for basic::error::output::Operator

--- test/error/Definition.1.main.cpp
+++ test/error/Definition.1.main.cpp
@@ -5,84 +5,157 @@ BASIC_TEST_CONSTRUCT;
 
 #include <typeinfo>
 #include <utility>
+#include <sstream>
+#include <string>
+
+using Case1 = basic::test::type::
+    Parameter<TestIsSameType>;
+using Case2 = basic::test::type::
+    Parameter<TestCallOutputOperator>;
 
 typedef VariableIsSameType<basic::error::defn::type::Char,
-    BASIC_ERROR_CHAR_TYPE> Var1Type;
+    BASIC_ERROR_CHAR_TYPE> T1Var1;
 
-Var1Type var1{"basic::error::defn::type::Char", "BASIC_ERROR_CHAR_TYPE"};
+T1Var1 t1_var1{"basic::error::defn::type::Char", "BASIC_ERROR_CHAR_TYPE"};
 
-REGISTER_TEST(t1, new TestDefinition<Var1Type>(var1));
+REGISTER_TEST(t1, new TestDefinition<Case1, 
+    typename T1Var1::BaseType>(t1_var1));
 
 typedef VariableIsSameType<basic::error::defn::type::Output,
-    BASIC_ERROR_OUTPUT_TYPE> Var2Type;
+    BASIC_ERROR_OUTPUT_TYPE> T2Var1;
 
-Var2Type var2{"basic::error::defn::type::Char", "BASIC_ERROR_OUTPUT_TYPE"};
+T2Var1 t2_var1{"basic::error::defn::type::Char", "BASIC_ERROR_OUTPUT_TYPE"};
 
-REGISTER_TEST(t2, new TestDefinition<Var2Type>(var2));
+REGISTER_TEST(t2, new TestDefinition<Case1,
+    typename T2Var1::BaseType>(t2_var1));
 
 typedef VariableIsSameType<basic::error::defn::type::code::Value,
-    BASIC_ERROR_CODE_VALUE_TYPE> Var3Type;
+    BASIC_ERROR_CODE_VALUE_TYPE> T3Var1;
 
-Var3Type var3{"basic::error::defn::type::code::Value", 
+T3Var1 t3_var1{"basic::error::defn::type::code::Value", 
     "BASIC_ERROR_CODE_VALUE_TYPE"};
 
-REGISTER_TEST(t3, new TestDefinition<Var3Type>(var3));
+REGISTER_TEST(t3, new TestDefinition<Case1,
+    typename T3Var1::BaseType>(t3_var1));
 
 typedef VariableIsSameType<basic::error::defn::type::id::Record,
-    BASIC_ERROR_IDENTIFICATION_RECORD_TYPE> Var4Type;
+    BASIC_ERROR_IDENTIFICATION_RECORD_TYPE> T4Var1;
 
-Var4Type var4{"basic::error::defn::type::id::Record", 
+T4Var1 t4_var1{"basic::error::defn::type::id::Record", 
     "BASIC_ERROR_IDENTIFICATION_RECORD_TYPE"};
 
-REGISTER_TEST(t4, new TestDefinition<Var4Type>(var4));
+REGISTER_TEST(t4, new TestDefinition<Case1,
+    typename T4Var1::BaseType>(t4_var1));
 
 typedef VariableIsSameType<basic::error::defn::type::msg::str::Storage,
-    BASIC_ERROR_MSG_STR_STORAGE_TYPE> Var5Type;
+    BASIC_ERROR_MSG_STR_STORAGE_TYPE> T5Var1;
 
-Var5Type var5{"basic::error::defn::type::msg::str::Storage", 
+T5Var1 t5_var1{"basic::error::defn::type::msg::str::Storage", 
     "BASIC_ERROR_MSG_STR_STORAGE_TYPE"};
 
-REGISTER_TEST(t5, new TestDefinition<Var5Type>(var5));
+REGISTER_TEST(t5, new TestDefinition<Case1,
+    typename T5Var1::BaseType>(t5_var1));
 
 typedef VariableIsSameType<basic::error::defn::type::output::Char,
-    BASIC_ERROR_CHAR_TYPE> Var6Type;
+    BASIC_ERROR_CHAR_TYPE> T6Var1;
 
-Var6Type var6{"basic::error::defn::type::output::Char", 
+T6Var1 t6_var1{"basic::error::defn::type::output::Char", 
     "BASIC_ERROR_CHAR_TYPE"};
 
-REGISTER_TEST(t6, new TestDefinition<Var6Type>(var6));
+REGISTER_TEST(t6, new TestDefinition<Case1,
+    typename T6Var1::BaseType>(t6_var1));
 
 typedef VariableIsSameType<basic::error::defn::type::output::CharTrait,
-    BASIC_ERROR_OUTPUT_CHARTRAIT_TYPE> Var7Type;
+    BASIC_ERROR_OUTPUT_CHARTRAIT_TYPE> T7Var1;
 
-Var7Type var7{"basic::error::defn::type::output::CharTrait", 
+T7Var1 t7_var1{"basic::error::defn::type::output::CharTrait", 
     "BASIC_ERROR_OUTPUT_CHARTRAIT_TYPE"};
 
-REGISTER_TEST(t7, new TestDefinition<Var7Type>(var7));
+REGISTER_TEST(t7, new TestDefinition<Case1,
+    typename T7Var1::BaseType>(t7_var1));
 
 typedef VariableIsSameType<basic::error::defn::type::system::category::Value,
-    BASIC_ERROR_SYSTEM_CATEGORY_VALUE_TYPE> Var8Type;
+    BASIC_ERROR_SYSTEM_CATEGORY_VALUE_TYPE> T8Var1;
 
-Var8Type var8{"basic::error::defn::type::system::category::Value", 
+T8Var1 t8_var1{"basic::error::defn::type::system::category::Value", 
     "BASIC_ERROR_SYSTEM_CATEGORY_VALUE_TYPE"};
 
-REGISTER_TEST(t8, new TestDefinition<Var8Type>(var8));
+REGISTER_TEST(t8, new TestDefinition<Case1,
+    typename T8Var1::BaseType>(t8_var1));
 
 typedef VariableIsSameType<basic::error::defn::type::system::code::Value,
-    BASIC_ERROR_SYSTEM_CODE_VALUE_TYPE> Var9Type;
+    BASIC_ERROR_SYSTEM_CODE_VALUE_TYPE> T9Var1;
 
-Var9Type var9{"basic::error::defn::type::system::code::Value", 
+T9Var1 t9_var1{"basic::error::defn::type::system::code::Value", 
     "BASIC_ERROR_SYSTEM_CODE_VALUE_TYPE"};
 
-REGISTER_TEST(t9, new TestDefinition<Var9Type>(var9));
+REGISTER_TEST(t9, new TestDefinition<Case1,
+    typename T9Var1::BaseType>(t9_var1));
 
 typedef VariableIsSameType<basic::error::defn::type::system::condition::Value,
-    BASIC_ERROR_SYSTEM_CODE_VALUE_TYPE> Var10Type;
+    BASIC_ERROR_SYSTEM_CODE_VALUE_TYPE> T10Var1;
 
-Var10Type var10{"basic::error::defn::type::system::condition::Value", 
+T10Var1 t10_var1{"basic::error::defn::type::system::condition::Value", 
     "BASIC_ERROR_SYSTEM_CONDITION_VALUE_TYPE"};
 
-REGISTER_TEST(t10, new TestDefinition<Var10Type>(var10));
+REGISTER_TEST(t10, new TestDefinition<Case1,
+    typename T10Var1::BaseType>(t10_var1));
+
+typedef VariableCallOutputOperator<BASIC_ERROR_OUTPUT_TYPE, 
+    std::stringstream, const char *> T11Var1;
+    
+typedef VariableCallOutputOperator<BASIC_ERROR_OUTPUT_TYPE, 
+    std::stringstream, const char *, const char *, int> T11Var2;
+    
+typedef VariableCallOutputOperator<BASIC_ERROR_OUTPUT_TYPE, 
+    std::stringstream, const char *, const char *, int, const char *, 
+    float> T11Var3;
+
+const char str1[] = "str1";
+int int1 = 4;
+float float1 = 3.14;
+std::stringstream strstream1;
+std::stringstream strstream2;
+std::stringstream strstream3;
+
+bool Result1(std::stringstream & out)
+{
+    return out.str().compare(str1) == 0;
+}
+
+bool Result2(std::stringstream & out)
+{
+    std::string l_str;
+    int l_int;
+    out >> l_str >> l_int;
+    return l_str.compare(str1) == 0 && l_int == int1;
+}
+
+bool Result3(std::stringstream & out)
+{
+    std::string l_str;
+    int l_int;
+    float l_float;
+    out >> l_str >> l_int >> l_float;
+    return l_str.compare(str1) == 0 && l_int == int1 && l_float == float1;
+}
+
+T11Var1 t11_var1{"basic::error::output::Operator", 
+    basic::error::output::Operator, "Result1", Result1, 
+    strstream1, std::move(str1)};
+
+T11Var2 t11_var2{"basic::error::output::Operator", 
+    basic::error::output::Operator, "Result2", Result2, strstream2, 
+    std::move(str1), std::move(" "), std::move(int1)};
+
+T11Var3 t11_var3{"basic::error::output::Operator", 
+    basic::error::output::Operator, "Result3", Result3, strstream3, 
+    std::move(str1), std::move(" "), std::move(int1), 
+    std::move(" "), std::move(float1)};
+
+REGISTER_TEST(t11, new TestDefinition<Case2, 
+    typename T11Var1::BaseType, typename T11Var2::BaseType,
+    typename T11Var3::BaseType>(t11_var1, t11_var2, t11_var3));
 
 int main()
 {
