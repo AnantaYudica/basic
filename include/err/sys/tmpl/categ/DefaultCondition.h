c73dfb358f545c3a49ec5719c16db82d233f1bbf
AnantaYudica <nta040689@gmail.com>
2019-04-02 13:39:29 +0700

    Fixed mistake using definition code value header

--- include/err/sys/tmpl/categ/DefaultCondition.h
+++ include/err/sys/tmpl/categ/DefaultCondition.h
@@ -6,7 +6,7 @@
 #include "has/mmbr/func/DefaultCondition.h"
 #include "../../Condition.defn.h"
 #include "../../Code.defn.h"
-#include "../../../defn/type/code/Value.h"
+#include "../../../defn/type/sys/code/Value.h"
 
 #include <type_traits>
 #include <utility>
@@ -36,12 +36,13 @@ DefaultCondition(const TCategoryTrait & categ_trait, const TCode & code,
 template<typename TCategoryTrait>
 typename std::enable_if<
     !has::mmbr::func::DefaultCondition<TCategoryTrait, sys::Condition, 
-        err::defn::type::code::Value, tmpl::Category<TCategoryTrait>>::Value &&
+        err::defn::type::sys::code::Value, 
+        tmpl::Category<TCategoryTrait>>::Value &&
     has::mmbr::func::DefaultCondition<TCategoryTrait, sys::Condition, 
         sys::Code, tmpl::Category<TCategoryTrait>>::Value, 
     sys::Condition>::type  
 DefaultCondition(const TCategoryTrait & categ_trait, 
-    const err::defn::type::code::Value & code,
+    const err::defn::type::sys::code::Value & code,
     const tmpl::Category<TCategoryTrait> & categ_instance) noexcept
 {
     return std::move(categ_trait.template DefaultCondition<
@@ -51,7 +52,8 @@ DefaultCondition(const TCategoryTrait & categ_trait,
 template<typename TCategoryTrait>
 typename std::enable_if<
     has::mmbr::func::DefaultCondition<TCategoryTrait, sys::Condition, 
-        err::defn::type::code::Value, tmpl::Category<TCategoryTrait>>::Value &&
+        err::defn::type::sys::code::Value, 
+        tmpl::Category<TCategoryTrait>>::Value &&
     !has::mmbr::func::DefaultCondition<TCategoryTrait, sys::Condition, 
         sys::Code, tmpl::Category<TCategoryTrait>>::Value, 
     sys::Condition>::type  
@@ -65,7 +67,8 @@ DefaultCondition(const TCategoryTrait & categ_trait, const sys::Code & code,
 template<typename TCategoryTrait>
 typename std::enable_if<
     !has::mmbr::func::DefaultCondition<TCategoryTrait, sys::Condition, 
-        err::defn::type::code::Value, tmpl::Category<TCategoryTrait>>::Value &&
+        err::defn::type::sys::code::Value, 
+        tmpl::Category<TCategoryTrait>>::Value &&
     !has::mmbr::func::DefaultCondition<TCategoryTrait, sys::Condition, 
         sys::Code, tmpl::Category<TCategoryTrait>>::Value,
     sys::Condition>::type  
@@ -78,7 +81,8 @@ DefaultCondition(const TCategoryTrait & categ_trait, const sys::Code & code,
 template<typename TCategoryTrait, typename TCode>
 typename std::enable_if<
     !has::mmbr::func::DefaultCondition<TCategoryTrait, sys::Condition, 
-        err::defn::type::code::Value, tmpl::Category<TCategoryTrait>>::Value &&
+        err::defn::type::sys::code::Value, 
+        tmpl::Category<TCategoryTrait>>::Value &&
     !has::mmbr::func::DefaultCondition<TCategoryTrait, sys::Condition, 
         sys::Code, tmpl::Category<TCategoryTrait>>::Value, 
     sys::Condition>::type  
